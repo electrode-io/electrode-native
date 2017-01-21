0cdceb154ee55204dc16a316af2fded31514ea7a
Justin Spears <jspears@walmartlabs.com>
2017-01-20 16:09:45 -0800

    Fix it

--- ern-cauldron-api/src/db.js
+++ ern-cauldron-api/src/db.js
@@ -2,25 +2,28 @@ import fs from 'fs';
 import path from 'path';
 import mkdirp from 'mkdirp';
 
+function json(path) {
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+}
+
 export default class Db {
-  constructor(dbPath) {
-    try {
-      fs.statSync(dbPath);
-    } catch (e) {
-      mkdirp(path.dirname(dbPath));
-      console.log("-");
-      fs.writeFileSync(dbPath, '{"nativeApps":[], "reactNativeApps":[]}');
+    constructor(dbPath) {
+        try {
+            fs.statSync(dbPath);
+        } catch (e) {
+            mkdirp(path.dirname(dbPath));
+            console.log("-");
+            fs.writeFileSync(dbPath, '{"nativeApps":[], "reactNativeApps":[]}');
+        }
+        this._dbPath = dbPath;
+        this._cauldron = json(this._dbPath);
     }
 
-    this._cauldron = require(dbPath);
-    this._dbPath = dbPath;
-  }
+    get cauldron() {
+        return this._cauldron = json(this._dbPath);
+    }
 
-  get cauldron() {
-    return this._cauldron;
-  }
-
-  commit() {
-    fs.writeFileSync(this._dbPath, JSON.stringify(this._cauldron));
-  }
+    commit() {
+        fs.writeFileSync(this._dbPath, JSON.stringify(this._cauldron));
+    }
 };
