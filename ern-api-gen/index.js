749f0c693ba81baed1b7ead63ba0a4fe0799620b
Benoit Lemaire <blemaire@walmartlabs.com>
2017-02-06 15:09:33 -0800

    Minor update to api gen

--- ern-api-gen/index.js
+++ ern-api-gen/index.js
@@ -714,55 +714,56 @@ export default async function generateApi({
         //
         // Java specific view
         const javaView = {
-            // JAVA code to use for payload deserialization
-            "payloadDeserialization": function () {
-                if (!this.payload) {
-                    return "null";
-                }
-                // Array
-                if (isArrayType(this.payload.type)) {
-                    const arrayType = getArrayType(this.payload.type);
-                    // Array of a primitive type
-                    if (primitiveTypes.includes(arrayType)) {
-                        let objType = (arrayType === 'Integer' ? 'Int' : arrayType);
-                        return `bundle.get${objType}Array("${this.payload.name}")`;
-                    }
-                    // Array of a complex object type
-                    else {
-                        throw new Error("Complex object type arrays are not supported yet");
-                    }
-                }
-                // No array
-                else {
-                    if (primitiveTypes.includes(this.payload.type)) {
-                        const primType = (this.payload.type === 'Integer' ? 'Int' : this.payload.type);
-                        return `bundle.get${primType}("${this.payload.name}")`;
-                    } else {
-                        return `${this.payload.type}.fromBundle(bundle)`;
-                    }
-                }
-            },
-            // JAVA code to use for payload serialization (request payload / event payload)
-            "payloadSerizalization": function () {
-                // Array
-                if (isArrayType(this.payload.type)) {
-                    const arrayType = getArrayType(this.payload.type);
-                    // Array of a primitive type
-                    if (primitiveTypes.includes(arrayType)) {
-                        let objType = (arrayType === 'Integer' ? 'Int' : arrayType);
-                        return `new Bundle(); bundle.put${objType}Array("${this.payload.name}", \
-                  ${this.payload.name});`;
-                    }
-                    // Array of a complex object type
-                    else {
-                        throw new Error("Complex object type arrays are not supported yet");
-                    }
-                }
-                // Not Array
-                else {
-                    if (primitiveTypes.includes(this.payload.type)) {
-                        const primType = (this.payload.type === 'Integer' ? 'Int' : this.payload.type);
-                        return `new Bundle(); bundle.put${primType}("${this.payload.name}",\
+          // JAVA code to use for payload deserialization
+          "payloadDeserialization": function() {
+            if (!this.payload) {
+             return "Object payload = null;";
+            }
+            // Array
+            if (isArrayType(this.payload.type)) {
+              const arrayType = getArrayType(this.payload.type);
+              // Array of a primitive type
+              if (primitiveTypes.includes(arrayType)) {
+                let objType = (arrayType === 'Integer' ? 'Int' : arrayType);
+                return `${this.payload.type} payload = bundle.get${objType}Array("${this.payload.name}");`;
+              }
+              // Array of a complex object type
+              else {
+                return `Parcelable[] p = bundle.getParcelableArray("${this.payload.name}");
+                        ${this.payload.type} payload = new ${arrayType}[p.length];
+                        System.arraycopy(p, 0, payload, 0, p.length);`;
+              }
+            }
+            // No array
+            else {
+              if (primitiveTypes.includes(this.payload.type)) {
+                const primType = (this.payload.type === 'Integer' ? 'Int' : this.payload.type);
+               return `${this.payload.type} payload = bundle.get${primType}("${this.payload.name}");`;
+              } else {
+               return `${this.payload.type} payload = ${this.payload.type}.fromBundle(bundle);`;
+              }
+            }
+          },
+          // JAVA code to use for payload serialization (request payload / event payload)
+          "payloadSerizalization": function() {
+            // Array
+            if (isArrayType(this.payload.type)) {
+              const arrayType = getArrayType(this.payload.type);
+              // Array of a primitive type
+              if (primitiveTypes.includes(arrayType)) {
+                let objType = (arrayType === 'Integer' ? 'Int' : arrayType);
+                return `new Bundle(); bundle.put${objType}Array("${this.payload.name}", ${this.payload.name});`;
+              }
+              // Array of a complex object type
+              else {
+                return `new Bundle(); bundle.putParcelableArray("${this.payload.name}", ${this.payload.name});`;
+              }
+            }
+            // Not Array
+            else {
+              if (primitiveTypes.includes(this.payload.type)) {
+               const primType = (this.payload.type === 'Integer' ? 'Int' : this.payload.type);
+               return `new Bundle(); bundle.put${primType}("${this.payload.name}",\
                    ${this.payload.name});`;
                     } else {
                         return `${this.payload.name}.toBundle();`;
@@ -805,7 +806,7 @@ export default async function generateApi({
                     // Array of a primitive type
                     if (primitiveTypes.includes(arrayType)) {
                         let objType = (arrayType === 'Integer' ? 'Int' : arrayType);
-                        return `bundle.get${objType}Array("rsp")`;
+                        return `${this} payload = bundle.get${objType}Array("rsp");`;
                     }
                     // Array of a complex object type
                     else {
