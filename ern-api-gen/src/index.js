c5305c2a6de26744d4b70ce1aea0c3d373e94092
Benoit Lemaire <blemaire@walmartlabs.com>
2017-02-14 12:04:03 -0800

    Improve api gen command line support

--- ern-api-gen/src/index.js
+++ ern-api-gen/src/index.js
@@ -1,22 +1,22 @@
 import runModelGen from "../../ern-model-gen/index.js";
 import generateJavaCode from './generateJavaCode';
 import generateObjectiveCCode from './generateObjectiveCCode';
-import generatePackage from './generateProject';
+import generateProject from './generateProject';
 import generateJSCode from './generateJSCode';
-import {patchHull} from './renderer';
+import { patchHull } from './renderer';
 import normalizeConfig from './normalizeConfig';
 import views from './views';
 import fs from 'fs';
 import shell from 'shelljs';
 import path from 'path';
 import {
-    SCHEMA_FILE,
-    PKG_FILE,
-    MODEL_FILE
+  SCHEMA_FILE,
+  PKG_FILE,
+  MODEL_FILE
 } from './Constants';
 import cwd from './cwd';
 import log from './log';
-import {readJSON} from './fileUtil';
+import { readJSON } from './fileUtil';
 
 // Not pretty but depending of the execution context (direct call to binary
 // v.s using api-gen in a node program) the path might include distrib
@@ -29,131 +29,112 @@ const apiGenDir = path.join(__dirname, '..');
  * view : the mustache view to use
  */
 async function generateAllCode(view) {
-    await generateJavaCode(view, apiGenDir);
-    await generateObjectiveCCode(view, apiGenDir);
-    await generateJSCode(view, apiGenDir);
+  await generateJavaCode(view, apiGenDir);
+  await generateObjectiveCCode(view, apiGenDir);
+  await generateJSCode(view, apiGenDir);
 }
-
 
 function hasModelSchema(modelsSchemaPath = cwd(MODEL_FILE)) {
-    return fs.existsSync(modelsSchemaPath) && modelsSchemaPath;
+  return fs.existsSync(modelsSchemaPath) && modelsSchemaPath;
 }
-
 
 /**
  * ==============================================================================
  * Main entry point
  * ==============================================================================
  *
- *
- * schemaFilePath : path to a schema file to be used as input (OPTIONAL)
- * configFilePath : path to a config file to be used as input (OPTIONAL)
- * shouldPublishToNpm : true to publish to npm after generation, false otherwise
+ * Refer to normalizeConfig function doc for the list of options
  */
 export async function generateApi(options) {
+  let config = normalizeConfig(options);
 
-    //--------------------------------------------------------------------------
-    // Get input
-    //--------------------------------------------------------------------------
-    // Two ways to provide needed input to apigen :
-    // - Through a schema file (in that case, a config object will be generated
-    // from the schema)
-    // - Directly giving a config file, skipping schema conversion
-    //
-    // Order of precedence when trying to get input :
-    // - schema from schemaFilePath (provided by apigen method caller)
-    // - config from configFilePath (provided by apigen method caller)
-    // - default schema file
-    // - default config file
+  const outFolder = cwd(config.moduleName);
+  if (fs.existsSync(outFolder)) {
+    log.warn(`A directory already exists at ${outFolder}`);
+    process.exit(1);
+  }
 
-    let config = normalizeConfig(options);
-    const outFolder = cwd(config.moduleName);
-    if (fs.existsSync(outFolder)) {
-        log.warn(`A directory already exists at ${outFolder}`);
-        process.exit(1);
-    }
-    // Create output folder
-    shell.mkdir(outFolder);
-    await generatePackage(config, outFolder);
+  // Create output folder
+  shell.mkdir(outFolder);
+  await generateProject(config, outFolder);
 
-    log.info(`==  Generated project: 
+  log.info(`==  Generated project: 
         $ cd ${outFolder}
         $ yarn install
         `);
 }
-export async function cleanGenerated(outFolder = cwd()) {
-    const pkg = await checkValid(`Is this not an api directory try a directory named: react-native-{name}-api`);
 
-    shell.rm('-rf', path.join(outFolder, 'js'));
-    shell.rm('-rf', path.join(outFolder, 'ios'));
-    shell.rm('-rf', path.join(outFolder, 'android'));
-    shell.rm('-f', path.join(outFolder, 'index.js'));
-    return pkg;
+export async function cleanGenerated(outFolder = cwd()) {
+  const pkg = await checkValid(`Is this not an api directory try a directory named: react-native-{name}-api`);
+
+  shell.rm('-rf', path.join(outFolder, 'js'));
+  shell.rm('-rf', path.join(outFolder, 'ios'));
+  shell.rm('-rf', path.join(outFolder, 'android'));
+  shell.rm('-f', path.join(outFolder, 'index.js'));
+  return pkg;
 }
 
 async function checkValid(message) {
+  const outFolder = cwd();
 
-    const outFolder = cwd();
+  if (!/react-native-(.*)-api$/.test(outFolder) || !fs.existsSync(cwd(SCHEMA_FILE))) {
+    throw new Error(message);
+  }
+  let pkg;
+  try {
+    pkg = await readJSON(cwd(PKG_FILE));
+  } catch (e) {
+    throw new Error(message);
+  }
+  if (!/react-native-(.*)-api$/.test(pkg.name)) {
+    throw new Error(message);
+  }
 
-    if (!/react-native-(.*)-api$/.test(outFolder) || !fs.existsSync(cwd(SCHEMA_FILE))) {
-        throw new Error(message);
-    }
-    let pkg;
-    try {
-        pkg = await readJSON(cwd(PKG_FILE));
-    } catch (e) {
-        throw new Error(message);
-    }
-    if ( !/react-native-(.*)-api$/.test(pkg.name)) {
-        throw new Error(message);
-    }
-
-    return pkg;
+  return pkg;
 }
+
 export async function generateCode(options) {
-    log.info('== Regenerating Code');
-    const pkg = await cleanGenerated();
+  log.info('== Regenerating Code');
+  const pkg = await cleanGenerated();
 
-    const config = normalizeConfig(Object.assign({}, {
-        name: pkg.name,
-        apiVersion: pkg.version,
-        apiDescription: pkg.description,
-        apiAuthor: pkg.author
-    }, options));
+  const config = normalizeConfig(Object.assign({}, {
+    name: pkg.name,
+    apiVersion: pkg.version,
+    apiDescription: pkg.description,
+    apiAuthor: pkg.author
+  }, options));
 
-    const outFolder = cwd();
+  const outFolder = cwd();
 
+  // Copy the api hull (skeleton code with inline templates) to output folder
+  shell.cp('-r', `${apiGenDir}/api-hull/*`, outFolder);
 
-    // Copy the api hull (skeleton code with inline templates) to output folder
-    shell.cp('-r', `${apiGenDir}/api-hull/*`, outFolder);
+  //--------------------------------------------------------------------------
+  // Mustache view creation
+  //--------------------------------------------------------------------------
 
+  const mustacheView = await views(config, outFolder);
 
-    //--------------------------------------------------------------------------
-    // Mustache view creation
-    //-----------------------------------------------------------config.apiName---------------
+  //--------------------------------------------------------------------------
+  // Kickstart generation
+  //--------------------------------------------------------------------------
 
-    const mustacheView = await views(config, outFolder);
-    //--------------------------------------------------------------------------
-    // Kickstart generation
-    //--------------------------------------------------------------------------
+  // Start by patching the api hull "inplace"
+  log.info("== Patching Hull");
+  await patchHull(mustacheView);
+  // Inject all additional generated code
+  log.info("== Generating API code");
+  await generateAllCode(mustacheView);
 
-    // Start by patching the api hull "inplace"
-    log.info("== Patching Hull");
-    await patchHull(mustacheView);
-    // Inject all additional generated code
-    log.info("== Generating API code");
-    await generateAllCode(mustacheView);
+  const schemaPath = hasModelSchema(config.modelsSchemaPath);
 
-    const schemaPath = hasModelSchema(config.modelsSchemaPath);
-
-    if (schemaPath) {
-        await runModelGen({
-            javaModelDest: `${outFolder}/android/lib/src/main/java/${config.namespace.replace(/\./g, '/')}/${config.apiName}/model`,
-            javaPackage: `${config.namespace}.${config.apiName.toLowerCase()}.model`,
-            objCModelDest: `${outFolder}/ios/MODEL`,
-            schemaPath
-        });
-    }
-    log.info("== Generation complete");
-
+  if (schemaPath) {
+    await runModelGen({
+      javaModelDest: `${outFolder}/android/lib/src/main/java/${config.namespace.replace(/\./g, '/')}/${config.apiName}/model`,
+      javaPackage: `${config.namespace}.${config.apiName.toLowerCase()}.model`,
+      objCModelDest: `${outFolder}/ios/MODEL`,
+      schemaPath
+    });
+  }
+  log.info("== Generation complete");
 }
--- ern-api-gen/src/normalizeConfig.js
+++ ern-api-gen/src/normalizeConfig.js
@@ -1,92 +1,105 @@
 import cwd from './cwd';
 import parseApiSchema from './parseApiSchema';
 import fs from 'fs';
-import {SCHEMA_FILE, CONFIG_FILE} from './Constants';
+import { SCHEMA_FILE, CONFIG_FILE } from './Constants';
 
 function generateConfigFromSchemaSync(file) {
-    return parseApiSchema(fs.readFileSync(file, 'utf8'));
+  return parseApiSchema(fs.readFileSync(file, 'utf8'), file);
 }
 
-//generate a configuration.  This looks in the apigen schema
+// Generate a configuration. This looks in the apigen schema
 // and the things passed in.
 const isApiRe = /.*react-native-(.*)-api$/;
 const isNameRe = /^(?:([^@]*)@)?(.*)$/;
 
+//
+// Options :
+// - name : Name of the api [REQUIRED]
+// - bridgeVersion : The version of the bridge to use to generate API [REQUIRED]
+// - reactNativeVersion : Version of react native to use [REQUIRED]
+// - apiVersion : Version of the api [default: 1.0.0]
+// - apiDescription : Description of the API [default: ERN Generated API for {name}]
+// - apiAuthor : Author of the API [Default: EMAIL or USER env variable]
+// - namespace : Namespace to use for messages [Default: com.{npmscope}.{name}.ern]
+// - npmScope: Npm scope to use for the module [Default: no scope]
+// - modelsSchemaPath : Path to the file holding the models schema [Default : no path]
+// - apiSchemaPath : Path to the file holding the api schema [Default : no path]
+// - moduleName : Name of the generated npm module [Default : react-native-{name}-api]
+// - shouldGenerateBlankApi : Indicated whether to generate blank api/model schema or sample one [Default : false]
+// - configFilePath : Path to configuration file [Default : no path]
 export default function normalizeConfig({
-    name,
-    apiName,
-    apiVersion,
-    apiDescripion,
-    apiAuthor,
-    namespace,
-    npmScope,
-    bridgeVersion,
-    modelsSchemaPath,
-    moduleName,
-    reactNativeVersion,
-    schemaFilePath = cwd(SCHEMA_FILE),
-    configFilePath = cwd(CONFIG_FILE),
+  name, /* REQUIRED */
+  bridgeVersion, /* REQUIRED */
+  reactNativeVersion, /* REQUIRED */
+  apiVersion,
+  apiDescripion,
+  apiAuthor,
+  namespace,
+  npmScope,
+  modelsSchemaPath,
+  apiSchemaPath,
+  moduleName,
 
+  shouldGenerateBlankApi,
+  configFilePath = cwd(CONFIG_FILE),
 }) {
-    let simpleName = name;
+  let simpleName = name;
 
-    if (isApiRe.test(name)) {
-        simpleName = isApiRe.exec(name).pop();
-    }
+  if (isApiRe.test(name)) {
+    simpleName = isApiRe.exec(name).pop();
+  }
 
-    let config = {};
-    if (isNameRe.test(apiName)) {
-        const [all, nScope, nName] = isNameRe.exec(name);
-        if (!apiName) apiName = nName;
-        if (!npmScope) npmScope = nScope;
-    }
+  let config = {};
 
-    if (fs.existsSync(schemaFilePath)) {
-        Object.assign(config, generateConfigFromSchemaSync(schemaFilePath));
-    } else if (fs.existsSync(configFilePath)) {
-        Object.assign(JSON.parse(fs.readFileSync(configFilePath, 'utf-8')));
-    }
-    if (simpleName) {
-        config.apiName = simpleName;
-    }
-
-    if (namespace) {
-        config.namespace = namespace;
-    }
-    if (!config.namespace) {
-        config.namespace = npmScope ? `com.${npmScope}.${simpleName}.ern` : `com.${simpleName}.ern`
-    }
-    if (apiVersion) {
-        config.apiVersion = apiVersion;
-    }
-    if (apiDescripion) {
-        config.apiDescripion = apiDescripion;
-    }
-    if (npmScope) {
-        config.npmScope = npmScope;
-    }
-
-    if (!config.moduleName) {
-        config.moduleName = moduleName || `react-native-${simpleName}-api`;
-    }
-    if (!config.apiAuthor) {
-        config.apiAuthor = apiAuthor || process.env['EMAIL'] || process.env['USER']
-    }
-    if (!config.apiVersion) {
-        config.apiVersion = '1.0.0';
-    }
-    if (!config.apiDescripion) {
-        config.apiDescripion = `ERN Generated API for ${config.apiName}`;
-    }
-    if (modelsSchemaPath) {
-        config.modelsSchemaPath = modelsSchemaPath;
-    }
-    if (bridgeVersion) {
-        config.bridgeVersion = bridgeVersion;
-    }
-    if (reactNativeVersion) {
-        config.reactNativeVersion = reactNativeVersion;
-    }
-    return config;
+  if (fs.existsSync(apiSchemaPath)) {
+    Object.assign(config, generateConfigFromSchemaSync(apiSchemaPath));
+  } else if (fs.existsSync(configFilePath)) {
+    Object.assign(JSON.parse(fs.readFileSync(configFilePath, 'utf-8')));
+  }
+  if (simpleName) {
+    config.apiName = simpleName;
+  }
+  if (namespace) {
+    config.namespace = namespace;
+  }
+  if (!config.namespace) {
+    config.namespace = npmScope ? `com.${npmScope}.${simpleName}.ern` : `com.${simpleName}.ern`
+  }
+  if (apiVersion) {
+    config.apiVersion = apiVersion;
+  }
+  if (apiDescripion) {
+    config.apiDescripion = apiDescripion;
+  }
+  if (npmScope) {
+    config.npmScope = npmScope;
+  }
+  if (!config.moduleName) {
+    config.moduleName = moduleName || `react-native-${simpleName}-api`;
+  }
+  if (!config.apiAuthor) {
+    config.apiAuthor = apiAuthor || process.env['EMAIL'] || process.env['USER']
+  }
+  if (!config.apiVersion) {
+    config.apiVersion = '1.0.0';
+  }
+  if (!config.apiDescripion) {
+    config.apiDescripion = `ERN Generated API for ${config.apiName}`;
+  }
+  if (modelsSchemaPath) {
+    config.modelsSchemaPath = modelsSchemaPath;
+  }
+  if (bridgeVersion) {
+    config.bridgeVersion = bridgeVersion;
+  }
+  if (reactNativeVersion) {
+    config.reactNativeVersion = reactNativeVersion;
+  }
+  if (shouldGenerateBlankApi) {
+    config.shouldGenerateBlankApi = shouldGenerateBlankApi;
+  }
+  if (apiSchemaPath) {
+    config.apiSchemaPath = apiSchemaPath;
+  }
+  return config;
 }
-
