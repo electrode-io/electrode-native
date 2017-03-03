13378eec637e908dcb2dfbe04d01fa19c46dca0d
Benoit Lemaire <blemaire@walmartlabs.com>
2017-03-02 22:37:48 -0800

    Refactoring container gen to integrate iOS support

---
+++ ern-container-gen/hull/ios/ElectrodeContainer/ElectrodeBridgeDelegate.m
@@ -0,0 +1,40 @@
+//
+//  ElectrodeBridgeDelegate.m
+//  ElectrodeContainer
+//
+//  Created by Cody Garvin on 2/15/17.
+//  Copyright © 2017 Walmart. All rights reserved.
+//
+
+#import "ElectrodeBridgeDelegate.h"
+
+@interface ElectrodeBridgeDelegate ()
+@property (nonatomic, copy) NSURL *sourceURL;
+@property (nonatomic, strong) NSArray *extraModules;
+@end
+
+@implementation ElectrodeBridgeDelegate
+
+- (instancetype)initWithModuleURL:(NSURL *)url extraModules:(NSArray *)modules
+{
+    self = [super init];
+    if (self)
+    {
+        self.sourceURL = url;
+        self.extraModules = modules;
+    }
+    
+    return self;
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+    return _sourceURL;
+}
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
+{
+    return _extraModules;
+}
+
+@end
---
+++ ern-container-gen/hull/ios/ElectrodeContainer/ElectrodeReactNative_Internal.h
@@ -0,0 +1,21 @@
+//
+//  ElectrodeReactNative_Internal.h
+//  ElectrodeContainer
+//
+//  Created by Claire Weijie Li on 3/1/17.
+//  Copyright © 2017 Walmart. All rights reserved.
+//
+
+#import <TestReactContainer.h>
+
+@interface ElectrodeConfigure : NSObject <ElectrodePluginConfigurator>
+
+@property (nonatomic, assign, readonly) BOOL isDebugEnabled;
+
+@property (nonatomic, copy, readonly) NSString *codePushWithIDString;
+
+@property (nonatomic, copy, readonly) NSString *codePushWithServerURLString;
+
+- (instancetype)initWithData: (NSDictionary *)data;
+
+@end
