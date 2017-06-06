7731643c70d0e53c4957bac350782c89b867023c
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-06-06 11:00:00 -0700

    Api-Gen implementation WIP.

--- ern-api-impl-gen/src/index.js
+++ ern-api-impl-gen/src/index.js
@@ -1,96 +1,81 @@
 // @flow
 import fs from 'fs'
-import chalk from 'chalk'
 import shell from 'shelljs'
 
 import {
   Platform,
-  yarn,
-  spin
+  Dependency,
+  Utils
 } from '@walmart/ern-util'
 
-const {yarnAdd, yarnInstall} = yarn
+import ApiImplGen from './generators/ApiImplGen'
 
 const WORKING_FOLDER = `${Platform.rootDirectory}/api-impl-gen`
 const PLUGIN_FOLDER = `${WORKING_FOLDER}/plugins`
-const OUT_FOLDER = `${WORKING_FOLDER}/out`
+
+const platformPath = `${Platform.currentPlatformVersionPath}`
+
+// Contains all interesting folders paths
+const paths = {}
+
+// Where the container project hull is stored
+paths.apiImplHull = `${platformPath}/ern-api-impl-gen/hull`
+
+// Where the container generation configuration of all plugins is stored
+paths.pluginsConfig = `${platformPath}/ern-container-gen/plugins`
+
+// Where we download plugins
+paths.pluginsDownloadFolder = PLUGIN_FOLDER
+
+paths.platformPath = platformPath
 
 export async function generateApiImpl ({
-                                         apiName,
+                                         api,
                                          outputFolder,
                                          nativeOnly
                                        }: {
-  apiName: string,
+  api: string, // Can be an npm package, git repo link or a file location.
   outputFolder: string,
   nativeOnly: boolean
 } = {}) {
   console.log('Entering generate API IMPL')
 
-  if (!outputFolder) {
-    outputFolder = `${shell.pwd()}/${apiName}-impl`
-  }
+  // get the folder to output the generated project.
+  paths.outFolder = formOutputFolderName(api, outputFolder)
 
   if (fs.existsSync(outputFolder)) {
-    log.warn(chalk.red(`An implementation directory already exists: ${outputFolder}`))
-    process.exit(1)
+    Utils.logErrorAndExitProcess(`An implementation directory already exists: ${outputFolder}`)
+    // RETURN
   }
 
-  // Folder from which api-impl-gen is executed.
+  let platforms = getPlatforms(nativeOnly)
 
   // Creates a working folder to collect all the necessary files/folders for the api-impl generation.
   await createWorkingFolder()
 
-  // npm install the api code, this will be copied over to the generated project
-  await downloadApi(apiName)
-
-  // Generate the final project
-  await fillHull(outputFolder, apiName, nativeOnly)
-
-  // Copy the final content from working directory here
-  log.info(chalk.green(`${outputFolder} is generated for api: ${apiName}`))
+  new ApiImplGen().generateApiImplementation(api, paths, platforms)
 }
 
 async function createWorkingFolder () {
   shell.rm('-rf', WORKING_FOLDER)
   shell.mkdir('-p', PLUGIN_FOLDER)
-  shell.mkdir('-p', OUT_FOLDER)
 }
 
-async function fillHull (outputFolder, apiName, nativeOnly) {
-  try {
-    shell.mkdir(outputFolder)
-    if (nativeOnly) {
-      await spin(`generating android project`, fillHullAndroid(outputFolder, apiName))
-      await fillHullIos(outputFolder, apiName)
-    } else {
-      console.log(chalk.yellow(`TODO: Generate JS only project.`))
-    }
-  } catch (e) {
-    log.error(`error while filling api-impl-gen hull: ${e}`)
+function formOutputFolderName (api, outputFolder) {
+  let apiDependencyObj = Dependency.fromString(api)
+
+  if (!outputFolder) {
+    outputFolder = `${shell.pwd()}/${apiDependencyObj.name}-impl`
+  } else {
+    outputFolder = `${outputFolder}/${apiDependencyObj.name}-impl`
   }
+  return outputFolder
 }
 
-function fillHullAndroid (outputFolder, apiName) {
-  return new Promise((resolve, reject) => {
-    resolve(() => {
-      console.log(`[=== Starting api-impl-gen hull filling ===]`)
-      const androidFolder = `${outputFolder}/android`
-      shell.mkdir(androidFolder)
-      console.log(`Creating out folder and copying api-impl-gen Hull to it`)
-      shell.cp(`-R`, `${Platform.currentPlatformVersionPath}/ern-api-impl-gen/hull/android/*`, `${androidFolder}`)
-
-      console.log(`copying api code`)
-      shell.cp(`-R`, `${WORKING_FOLDER}/out/node_modules/${apiName}/android/lib/*`, `${androidFolder}/lib/`)
-    })
-  })
-}
-
-function fillHullIos (outputFolder, apiName) {
-  log.warn(chalk.yellow(`TODO: generate IOS impl project`))
-}
-
-async function downloadApi (apiName) {
-  shell.cd(OUT_FOLDER)
-  await yarnAdd(apiName)
-  await spin(`yarn installing ${apiName}`, yarnInstall())
+function getPlatforms (nativeOnly: boolean): Array<string> {
+  if (nativeOnly) {
+    return [`android`, `ios`]
+  } else {
+    return [`js`]
+  }
 }
