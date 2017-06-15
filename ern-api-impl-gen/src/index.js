0c36e624727b0a17b03765293e72112a31cc2684
Benoit Lemaire <blemaire@walmartlabs.com>
2017-06-15 10:06:49 -0700

    Update api-impl-gen code

--- ern-api-impl-gen/src/index.js
+++ ern-api-impl-gen/src/index.js
@@ -3,7 +3,6 @@ import fs from 'fs'
 import shell from 'shelljs'
 
 import {
-  Platform,
   Dependency,
   Utils
 } from '@walmart/ern-util'
@@ -11,40 +10,40 @@ import {
 import ApiImplGen from './generators/ApiImplGen'
 
 const API_NAME_RE = /([^/]*)$/
-const WORKING_FOLDER = `${Platform.rootDirectory}/api-impl-gen`
-const PLUGIN_FOLDER = `${WORKING_FOLDER}/plugins`
-
-const platformPath = `${Platform.currentPlatformVersionPath}`
 
 // Contains all interesting folders paths
 const paths = {}
 
-// Where the container project hull is stored
-paths.apiImplHull = `${platformPath}/ern-api-impl-gen/hull`
-
-// Where the container generation configuration of all plugins is stored
-paths.pluginsConfigPath = `${platformPath}/ern-container-gen/plugins`
-
-// Where we download plugins
-paths.pluginsDownloadFolder = PLUGIN_FOLDER
-
-paths.platformPath = platformPath
-
 export async function generateApiImpl ({
-                                         api,
-                                         outputFolder,
-                                         nativeOnly,
-                                         forceGenerate
-                                       }: {
+  api,
+  outputFolder,
+  nativeOnly,
+  forceGenerate,
+  platformPath,
+  workingFolder
+} : {
   api: string, // Can be an npm package, git repo link or a file location.
   outputFolder: string,
   nativeOnly: boolean,
-  forceGenerate: boolean
+  forceGenerate: boolean,
+  platformPath: string,
+  workingFolder: string
 } = {}) {
   console.log('Entering generate API IMPL')
 
+  paths.platformPath = platformPath
+
   // get the folder to output the generated project.
   paths.outFolder = outputFolder = formOutputFolderName(api, outputFolder)
+
+  // Where the container project hull is stored
+  paths.apiImplHull = `${platformPath}/ern-api-impl-gen/hull`
+
+  // Where the container generation configuration of all plugins is stored
+  paths.pluginsConfig = `${platformPath}/ern-container-gen/plugins`
+
+  // Where we download plugins
+  paths.pluginsDownloadFolder = `${workingFolder}/plugins`
 
   try {
     createOutputFolder(forceGenerate, outputFolder)
@@ -52,7 +51,7 @@ export async function generateApiImpl ({
     let platforms = getPlatforms(nativeOnly)
 
     // Creates a working folder to collect all the necessary files/folders for the api-impl generation.
-    await createWorkingFolder()
+    await createWorkingFolder(workingFolder)
 
     await new ApiImplGen().generateApiImplementation(api, paths, platforms)
   } catch (e) {
@@ -74,9 +73,9 @@ function createOutputFolder (forceGenerate, outputFolder) {
   }
 }
 
-async function createWorkingFolder () {
-  shell.rm('-rf', WORKING_FOLDER)
-  shell.mkdir('-p', PLUGIN_FOLDER)
+async function createWorkingFolder (path: string) {
+  shell.rm('-rf', path)
+  shell.mkdir('-p', `${path}/plugins`)
 }
 
 function formOutputFolderName (api, outputFolder) {
