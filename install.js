4cc7a6f041ebd9a7f4ec267cdc2e57cf0ddc61fa
Bharath M <bmarulasiddappa@walmartlabs.com>
2017-06-20 16:45:15 -0700

    Bump version to 0.1.1

--- install.js
+++ install.js
@@ -13,7 +13,7 @@ const fs = require('fs')
 // ....
 // |_ .ernrc
 
-const PLATFORM_VERSION = '0.1.0'
+const PLATFORM_VERSION = '0.1.1'
 // Path to ern platform root folder
 const ERN_PATH = process.env['ERN_HOME'] || `${process.env['HOME']}/.ern`
 // Path to ern platform cloned git repo
