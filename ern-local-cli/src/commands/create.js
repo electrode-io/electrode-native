931132c3922a905bf9816db43ce77beab834943c
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-07-05 13:47:14 -0700

    Fix command descriptions based on review feedbacks.!

--- ern-local-cli/src/commands/create.js
+++ ern-local-cli/src/commands/create.js
@@ -3,6 +3,6 @@
 exports.command = 'create'
 exports.desc = 'Commands to access platform creators'
 exports.builder = function (yargs: any) {
-  return yargs.commandDir('create').demandCommand(1, `generate needs a command`)
+  return yargs.commandDir('create').demandCommand(1, `create needs a command`)
 }
 exports.handler = function (argv: any) {}
