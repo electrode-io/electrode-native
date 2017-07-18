3112f2d2b6430f356659e34b628c6ab6633c4220
Benoit Lemaire <blemaire@walmartlabs.com>
2017-07-18 10:32:28 -0700

    Fix cauldron del dependency bug

--- ern-local-cli/src/commands/cauldron/del/dependency.js
+++ ern-local-cli/src/commands/cauldron/del/dependency.js
@@ -1,22 +1,23 @@
 // @flow
 
 import {
+  Dependency,
   NativeApplicationDescriptor
 } from '@walmart/ern-util'
 import cauldron from '../../../lib/cauldron'
 
-exports.command = 'dependency <completeNapDescriptor> <dependencyName>'
+exports.command = 'dependency <completeNapDescriptor> <dependency>'
 exports.desc = 'Remove a dependency from the cauldron'
 
 exports.builder = {}
 
 exports.handler = async function ({
   completeNapDescriptor,
-  dependencyName
+  dependency
 } : {
   completeNapDescriptor: string,
-  dependencyName: string
+  dependency: string
 }) {
   const napDescriptor = NativeApplicationDescriptor.fromString(completeNapDescriptor)
-  await cauldron.removeNativeDependency(napDescriptor, dependencyName)
+  await cauldron.removeNativeDependency(napDescriptor, Dependency.fromString(dependency))
 }
