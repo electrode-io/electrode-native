93ee75d31cde77ca67cffabe028c0701460787ea
Benoit Lemaire <blemaire@walmartlabs.com>
2017-08-17 10:32:51 -0700

    ern cauldron del miniapp now offers removing multiple miniapps at a time

--- ern-local-cli/src/commands/cauldron/del/miniapp.js
+++ ern-local-cli/src/commands/cauldron/del/miniapp.js
@@ -11,9 +11,11 @@ import {
   runCauldronContainerGen
 } from '../../../lib/publication'
 import semver from 'semver'
+import inquirer from 'inquirer'
+import _ from 'lodash'
 
-exports.command = 'miniapp <completeNapDescriptor> <miniAppName>'
-exports.desc = 'Remove a mini app from the cauldron'
+exports.command = 'miniapp [miniapp]'
+exports.desc = 'Remove one or more MiniApp(s) from the cauldron'
 
 exports.builder = function (yargs: any) {
   return yargs
@@ -22,6 +24,11 @@ exports.builder = function (yargs: any) {
     type: 'string',
     describe: 'Version to use for generated container. If none provided, patch version will be bumped by default.'
   })
+  .option('miniapps', {
+    type: 'array',
+    alias: 'm',
+    describe: 'A list of one or more miniapps'
+  })
 }
 
 // This command does not actually removes or offers to remove dependencies that were
@@ -29,22 +36,61 @@ exports.builder = function (yargs: any) {
 // It could be done as a future improvement to this command
 exports.handler = async function ({
   completeNapDescriptor,
-  miniAppName,
+  miniapp,
+  miniapps,
   containerVersion
 } : {
-  completeNapDescriptor: string,
-  miniAppName: string,
+  completeNapDescriptor?: string,
+  miniapp?: string,
+  miniapps?: Array<String>,
   containerVersion?: string
 }) {
   if (containerVersion) {
     ensureValidContainerVersion(containerVersion)
   }
 
+   //
+  // If no 'completeNapDescriptor' was provided, list all non released
+  // native application versions from the Cauldron, so that user can
+  // choose one of them to add the MiniApp(s) to
+  if (!completeNapDescriptor) {
+    const nativeApps = await cauldron.getAllNativeApps()
+
+    // Transform native apps from the cauldron to an Array
+    // of completeNapDescriptor strings
+    // [Should probably move to a Cauldron util class for reusability]
+    let result =
+    _.filter(
+      _.flattenDeep(
+        _.map(nativeApps, nativeApp =>
+          _.map(nativeApp.platforms, p =>
+            _.map(p.versions, version => {
+              if (!version.isReleased) {
+                return `${nativeApp.name}:${p.name}:${version.name}`
+              }
+            })))), elt => elt !== undefined)
+
+    const { userSelectedCompleteNapDescriptor } = await inquirer.prompt([{
+      type: 'list',
+      name: 'userSelectedCompleteNapDescriptor',
+      message: 'Choose a non released native application version in which you want to add this MiniApp',
+      choices: result
+    }])
+
+    completeNapDescriptor = userSelectedCompleteNapDescriptor
+  }
+
   const napDescriptor = NativeApplicationDescriptor.fromString(completeNapDescriptor)
 
-  const miniAppString = await cauldron.getContainerMiniApp(napDescriptor, miniAppName)
-  if (!miniAppString) {
-    return log.error(`${miniAppName} MiniApp is not present in ${completeNapDescriptor} container`)
+  const miniAppsAsDeps = miniapp
+    ? [ Dependency.fromString(miniapp) ]
+    : _.map(miniapps, m => Dependency.fromString(m))
+
+  for (const miniAppAsDep of miniAppsAsDeps) {
+    const miniAppString = await cauldron.getContainerMiniApp(napDescriptor, miniAppAsDep.toString())
+    if (!miniAppString) {
+      return log.error(`${miniAppAsDep.toString()} MiniApp is not present in ${completeNapDescriptor} container !`)
+    }
   }
 
   try {
@@ -62,7 +108,9 @@ exports.handler = async function ({
       log.debug(`Bumping container version from cauldron : ${cauldronContainerVersion}`)
     }
 
-    await cauldron.removeMiniAppFromContainer(napDescriptor, Dependency.fromString(miniAppName))
+    for (const miniAppAsDep of miniAppsAsDeps) {
+      await cauldron.removeMiniAppFromContainer(napDescriptor, miniAppAsDep)
+    }
 
     // Run container generator
     await runCauldronContainerGen(
@@ -76,10 +124,10 @@ exports.handler = async function ({
     // Commit Cauldron transaction
     await cauldron.commitTransaction()
 
-    log.info(`${miniAppString} MiniApp was succesfully removed from ${napDescriptor.toString()}`)
+    log.info(`MiniApp(s) was/were succesfully removed from ${napDescriptor.toString()}`)
     log.info(`Published new container version ${cauldronContainerVersion} for ${napDescriptor.toString()}`)
   } catch (e) {
-    log.error(`An error happened while trying to remove ${miniAppString} MiniApp from ${napDescriptor.toString()}`)
+    log.error(`An error happened while trying to remove MiniApp(s) from ${napDescriptor.toString()}`)
     cauldron.discardTransaction()
   }
 }
