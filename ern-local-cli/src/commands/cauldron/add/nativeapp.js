445c6fbd946fa4214dd8c1d17437cd6ea1ff5939
Benoit Lemaire <blemaire@walmartlabs.com>
2017-08-20 11:48:56 -0700

    Improve add nativeapp copyover robustness and speed, using transactions

--- ern-local-cli/src/commands/cauldron/add/nativeapp.js
+++ ern-local-cli/src/commands/cauldron/add/nativeapp.js
@@ -22,7 +22,8 @@ exports.builder = function (yargs: any) {
   })
   .option('copyPreviousVersionData', {
     alias: 'c',
-    describe: 'Copy previous version data'
+    describe: 'Copy previous version data',
+    type: 'boolean'
   })
 }
 
@@ -41,30 +42,40 @@ exports.handler = async function ({
 
   const napDescriptor = NativeApplicationDescriptor.fromString(completeNapDescriptor)
 
-  const previousApps = await cauldron.getNativeApp(new NativeApplicationDescriptor(napDescriptor.name, napDescriptor.platform))
+  try {
+    await cauldron.beginTransaction()
 
-  await cauldron.addNativeApp(napDescriptor, platformVersion
-    ? platformVersion.toString().replace('v', '')
-    : undefined)
+    const previousApps = await cauldron.getNativeApp(new NativeApplicationDescriptor(napDescriptor.name, napDescriptor.platform))
 
-  if (previousApps && (copyPreviousVersionData || await askUserCopyPreviousVersionData())) {
-    const previousNativeAppVersion = _.last(previousApps.versions)
-    // Copy over previous native application version native dependencies
-    for (const nativeDep of previousNativeAppVersion.nativeDeps) {
-      await cauldron.addNativeDependency(napDescriptor, Dependency.fromString(nativeDep))
+    await cauldron.addNativeApp(napDescriptor, platformVersion
+      ? platformVersion.toString().replace('v', '')
+      : undefined)
+
+    if (previousApps && (copyPreviousVersionData || await askUserCopyPreviousVersionData())) {
+      const previousNativeAppVersion = _.last(previousApps.versions)
+      // Copy over previous native application version native dependencies
+      for (const nativeDep of previousNativeAppVersion.nativeDeps) {
+        await cauldron.addNativeDependency(napDescriptor, Dependency.fromString(nativeDep))
+      }
+      // Copy over previous native application version container MiniApps
+      for (const containerMiniApp of previousNativeAppVersion.miniApps.container) {
+        await cauldron.addContainerMiniApp(napDescriptor, Dependency.fromString(containerMiniApp))
+      }
+      // Copy over previous yarn lock if any
+      if (previousNativeAppVersion.yarnlock) {
+        await cauldron.setYarnLockId(napDescriptor, previousNativeAppVersion.yarnlock)
+      }
+      // Copy over container version
+      if (previousNativeAppVersion.containerVersion) {
+        await cauldron.updateContainerVersion(napDescriptor, previousNativeAppVersion.containerVersion)
+      }
     }
-    // Copy over previous native application version container MiniApps
-    for (const containerMiniApp of previousNativeAppVersion.miniApps.container) {
-      await cauldron.addContainerMiniApp(napDescriptor, Dependency.fromString(containerMiniApp))
-    }
-    // Copy over previous yarn lock if any
-    if (previousNativeAppVersion.yarnlock) {
-      await cauldron.setYarnLockId(napDescriptor, previousNativeAppVersion.yarnlock)
-    }
-    // Copy over container version
-    if (previousNativeAppVersion.containerVersion) {
-      await cauldron.updateContainerVersion(napDescriptor, previousNativeAppVersion.containerVersion)
-    }
+
+    await cauldron.commitTransaction()
+    log.info(`${napDescriptor.toString()} was succesfuly added to the Cauldron`)
+  } catch (e) {
+    log.error(`An error occured while trying to add the native app to the Cauldron: ${e.message}`)
+    await cauldron.discardTransaction()
   }
 }
 
