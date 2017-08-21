9f8729b7199bfb1305d7254e9c43b3b37b598993
Benoit Lemaire <blemaire@walmartlabs.com>
2017-08-20 17:45:08 -0700

    Remove deprecated miniapp compat commands

--- ern-local-cli/src/commands/miniapp/compat.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-exports.command = 'compat'
-exports.desc = 'Commands related to binary compatibility checking of the miniapp'
-exports.builder = function (yargs: any) {
-  return yargs.commandDir('compat').demandCommand(1, 'compat needs a command')
-}
-exports.handler = function (argv: any) {}
