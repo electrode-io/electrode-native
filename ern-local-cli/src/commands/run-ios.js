76536331f856325e5eda54a2d6332b50d3587327
Benoit Lemaire <blemaire@walmartlabs.com>
2017-09-07 00:02:58 -0700

    Update run-ios and run-android commands

--- ern-local-cli/src/commands/run-ios.js
+++ ern-local-cli/src/commands/run-ios.js
@@ -2,30 +2,56 @@
 
 import utils from '../lib/utils'
 
-exports.command = 'run-ios [miniapp]'
-exports.desc = 'Run a MiniApp in the ios Runner application'
+exports.command = 'run-ios'
+exports.desc = 'Run one or more MiniApps in the iOS Runner application'
 
 exports.builder = function (yargs: any) {
   return yargs
     .option('dev', {
       type: 'bool',
-      alias: 'p',
-      default: true,
       describe: 'Enable or disable React Native dev support'
+    })
+    .option('miniapps', {
+      type: 'array',
+      alias: 'm',
+      describe: 'One or more MiniApps to combine in the Runner Container'
+    })
+    .option('dependencies', {
+      type: 'array',
+      alias: 'deps',
+      describe: 'One or more additional native dependencies to add to the Runner Container'
+    })
+    .option('descriptor', {
+      type: 'string',
+      alias: 'd',
+      describe: 'Full native application descriptor'
+    })
+    .option('mainMiniAppName', {
+      type: 'string',
+      describe: 'Name of the MiniApp to launch when starting the Runner application'
     })
     .epilog(utils.epilog(exports))
 }
 
 exports.handler = async function ({
-  miniapp,
-  dev = true
+  miniapps,
+  dependencies = [],
+  descriptor,
+  mainMiniAppName,
+  dev
 } : {
-  miniapp?: string,
-  dev: boolean
+  miniapps?: Array<string>,
+  dependencies: Array<string>,
+  descriptor?: string,
+  mainMiniAppName?: string,
+  dev?: boolean
 }) {
   try {
     await utils.runMiniApp('ios', {
-      miniapp,
+      mainMiniAppName,
+      miniapps,
+      dependencies,
+      descriptor,
       dev
     })
   } catch (e) {
