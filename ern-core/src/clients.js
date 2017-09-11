4c6a4ac0d6a63d4f16b6d9d44003e0293d3f111e
Benoit Lemaire <blemaire@walmartlabs.com>
2017-09-11 14:37:07 -0700

    Cleanup console.log

--- ern-core/src/clients.js
+++ ern-core/src/clients.js
@@ -13,6 +13,5 @@ export const reactnative = new ReactNativeCli(getBinaryPath('react-native'))
 export const codepush = new CodePushCli(getBinaryPath('code-push'))
 
 function getBinaryPath (binaryName: string) : string {
-  console.log(`dirname is ${__dirname}`)
   return path.resolve(__dirname, '..', 'node_modules', '.bin', binaryName)
 }
