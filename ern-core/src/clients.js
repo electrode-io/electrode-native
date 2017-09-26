438d225a884ab54cccdcb3c97a2c8fa0d3f8c1c5
Benoit Lemaire <blemaire@walmartlabs.com>
2017-09-25 23:03:22 -0700

    Properly handle difference in path structure to locate binaries

--- ern-core/src/clients.js
+++ ern-core/src/clients.js
@@ -5,6 +5,7 @@ import {
   ReactNativeCli,
   YarnCli
 } from 'ern-util'
+import fs from 'fs'
 import path from 'path'
 
 export const yarn = new YarnCli(getBinaryPath('yarn'))
@@ -12,5 +13,12 @@ export const reactnative = new ReactNativeCli(getBinaryPath('react-native'))
 export const codepush = new CodePushCli(getBinaryPath('code-push'))
 
 function getBinaryPath (binaryName: string) : string {
-  return path.resolve(__dirname, '..', 'node_modules', '.bin', binaryName)
+  const pathWhenInstalledWithYarn = path.resolve(__dirname, '..', 'node_modules', '.bin', binaryName)
+  const pathWhenInstalledWithNpm = path.resolve(__dirname, '..', '..', '.bin', binaryName)
+  if (fs.statSync(pathWhenInstalledWithYarn)) {
+    return pathWhenInstalledWithYarn
+  } else if (fs.statSync(pathWhenInstalledWithNpm)) {
+    return pathWhenInstalledWithNpm
+  }
+  throw new Error(`Could not find ${binaryName} in ${pathWhenInstalledWithYarn} nor in ${pathWhenInstalledWithNpm}`)
 }
