17a39c852fa08969e4f9c9cbf919a3c6ff2c3f33
Benoit Lemaire <blemaire@walmartlabs.com>
2017-09-25 23:34:46 -0700

    Use existsSync instead of statsSync

--- ern-core/src/clients.js
+++ ern-core/src/clients.js
@@ -15,9 +15,9 @@ export const codepush = new CodePushCli(getBinaryPath('code-push'))
 function getBinaryPath (binaryName: string) : string {
   const pathWhenInstalledWithYarn = path.resolve(__dirname, '..', 'node_modules', '.bin', binaryName)
   const pathWhenInstalledWithNpm = path.resolve(__dirname, '..', '..', '.bin', binaryName)
-  if (fs.statSync(pathWhenInstalledWithYarn)) {
+  if (fs.existsSync(pathWhenInstalledWithYarn)) {
     return pathWhenInstalledWithYarn
-  } else if (fs.statSync(pathWhenInstalledWithNpm)) {
+  } else if (fs.existsSync(pathWhenInstalledWithNpm)) {
     return pathWhenInstalledWithNpm
   }
   throw new Error(`Could not find ${binaryName} in ${pathWhenInstalledWithYarn} nor in ${pathWhenInstalledWithNpm}`)
