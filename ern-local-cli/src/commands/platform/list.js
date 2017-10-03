a0992fcfd287f4a87810093b42d952a37e8f6cea
Benoit Lemaire <blemaire@walmartlabs.com>
2017-10-03 15:40:38 -0700

    Add release notes link to ern platform list command

--- ern-local-cli/src/commands/platform/list.js
+++ ern-local-cli/src/commands/platform/list.js
@@ -9,6 +9,8 @@ import {
 import chalk from 'chalk'
 import utils from '../../lib/utils'
 
+const BASE_RELEASE_URL = `https://github.com/electrode-io/electrode-native/releases/tag`
+
 exports.command = 'list'
 exports.desc = 'List platform versions'
 
@@ -22,14 +24,18 @@ exports.handler = function () {
     ${chalk.yellow('[INSTALLED]')}
     ${chalk.gray('[NOT INSTALLED]')}`)
   for (const version of Platform.versions) {
+    // Don't show versions pre 0.7.0 as it was not official public releases
+    // Electrode Native initial public release was 0.7.0 so starting from
+    // this version
+    if (version < '0.7.0') { continue }
     if (Platform.isPlatformVersionInstalled(version)) {
       if (Platform.currentVersion === version) {
-        log.info(chalk.green(`-> v${version}`))
+        log.info(chalk.green(`-> v${version}\t\t`) + chalk.white(`${BASE_RELEASE_URL}/v${version}`))
       } else {
-        log.info(chalk.yellow(`v${version}`))
+        log.info(chalk.yellow(`v${version}\t\t`) + chalk.white(`${BASE_RELEASE_URL}/v${version}`))
       }
     } else {
-      log.info(chalk.gray(`v${version}`))
+      log.info(chalk.gray(`v${version}\t\t`) + chalk.white(`${BASE_RELEASE_URL}/v${version}`))
     }
   }
 }
