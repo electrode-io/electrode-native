3c46d4a3f93786b10e5ec314039f7e06e4ab13ea
Benoit Lemaire <blemaire@walmartlabs.com>
2017-10-03 21:47:48 -0700

    Introduce binarystore commands

---
+++ docs/cli/binarystore/add.md
@@ -0,0 +1,39 @@
+## `ern binarystore add <descriptor> <pathToBinary>`
+
+#### Description
+
+* Add a mobile application binary to the binary store
+
+#### Syntax
+
+`ern binarystore add <descriptor> <pathToBinary>`
+
+**Parameters**  
+
+`<descriptor>`
+
+A complete native application descriptor (ex: `myapp:android:1.0.0`), representing the mobile application version associated to this binary.
+
+`<pathToBinary>`
+
+Relative or absolute path to the binary to add to the binary store. For an `Android` binary the path should point to a `.apk` file, whereas of `iOS` it should point to a `.app` file.
+
+#### Remarks
+
+* This command will only work if the following conditions are met:
+  * A binary store server is running
+  * There is an active Cauldron
+  * The active Cauldron contains proper configuration of the binary store
+
+* If a binary already exists in the store for the targeted mobile application version, it will be replaced.
+
+#### Related commands
+
+ [ern binarystore get] | Get a mobile application binary from the binary store  
+ [ern binarystore remove] | Remove a mobile application binary from the binary store
+
+___  
+
+[ern binarystore get]: ./get.md
+
+[ern binarystore remove]: ./remove.md
---
+++ ern-local-cli/src/commands/binarystore/add.js
@@ -0,0 +1,56 @@
+// @flow
+
+import {
+  NativeApplicationDescriptor
+} from 'ern-util'
+import {
+  cauldron,
+  ErnBinaryStore
+} from 'ern-core'
+import utils from '../../lib/utils'
+import path from 'path'
+
+exports.command = 'add <descriptor> <pathToBinary>'
+exports.desc = 'Add a mobile application binary to the binary store'
+
+exports.builder = function (yargs: any) {
+  return yargs
+    .epilog(utils.epilog(exports))
+}
+
+exports.handler = async function ({
+  descriptor,
+  pathToBinary
+}: {
+  descriptor: string,
+  pathToBinary: string
+}) {
+  await utils.logErrorAndExitIfNotSatisfied({
+    cauldronIsActive: {
+      extraErrorMessage: 'A Cauldron must be active in order to use this command'
+    },
+    isCompleteNapDescriptorString: {
+      descriptor
+    },
+    napDescriptorExistInCauldron: {
+      descriptor,
+      extraErrorMessage: 'Cannot add binary of a non existing native application version'
+    }
+  })
+
+  const napDescriptor = NativeApplicationDescriptor.fromString(descriptor)
+
+  const binaryStoreConfig = await cauldron.getBinaryStoreConfig()
+  if (!binaryStoreConfig) {
+    return log.error('No binaryStore configuration was found in Cauldron')
+  }
+
+  try {
+    const binaryStore = new ErnBinaryStore(binaryStoreConfig)
+    const absolutePathToBinary = path.resolve(pathToBinary)
+    await binaryStore.addBinary(napDescriptor, absolutePathToBinary)
+    log.info(`Binary was successfuly added to the store for ${napDescriptor.toString()}`)
+  } catch (e) {
+    log.error(`An error happened while trying to add the binary of ${napDescriptor.toString()}`)
+  }
+}
