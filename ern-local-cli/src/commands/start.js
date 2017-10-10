cd295b7a6c5d52c3e643b190092fdba1acf81868
Benoit Lemaire <blemaire@walmartlabs.com>
2017-10-09 19:47:54 -0700

    Improve ern start command to launch app binary if available

--- ern-local-cli/src/commands/start.js
+++ ern-local-cli/src/commands/start.js
@@ -8,7 +8,7 @@ exports.desc = 'Start a composite MiniApp'
 
 exports.builder = function (yargs: any) {
   return yargs
-    .option('completeNapDescriptor', {
+    .option('descriptor', {
       type: 'string',
       alias: 'n',
       describe: 'Full native application selector'
@@ -23,20 +23,50 @@ exports.builder = function (yargs: any) {
       alias: 'w',
       describe: 'A list of one or more folder name from node_modules that should be watched for changes'
     })
+    .group(['packageName', 'activityName'], 'Android binary specific options:')
+    .option('packageName', {
+      type: 'string',
+      alias: 'p',
+      describe: 'Android application package name'
+    })
+    .option('activityName', {
+      type: 'string',
+      alias: 'a',
+      describe: 'Android Activity to launch'
+    })
+    .group(['bundleId'], 'iOS binary specific options:')
+    .option('bundleId', {
+      type: 'string',
+      alias: 'b',
+      describe: 'iOS Bundle Identifier'
+    })
     .epilog(utils.epilog(exports))
 }
 
 exports.handler = async function ({
-  completeNapDescriptor,
+  descriptor,
   miniapps,
-  watchNodeModules
+  watchNodeModules,
+  packageName,
+  activityName,
+  bundleId
 } : {
-  completeNapDescriptor?: string,
+  descriptor?: string,
   miniapps?: Array<string>,
-  watchNodeModules?: Array<string>
+  watchNodeModules?: Array<string>,
+  packageName?: string,
+  activityName?: string,
+  bundleId?: string
 }) {
   try {
-    await start({ miniapps, completeNapDescriptor, watchNodeModules })
+    await start({
+      miniapps,
+      descriptor,
+      watchNodeModules,
+      packageName,
+      activityName,
+      bundleId
+    })
   } catch (e) {
     log.error(e.message)
   }
