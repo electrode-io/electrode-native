e214abc12e4125e5db6dcbdcd3ac2341e6f7af32
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-10-25 14:37:40 -0700

    update isDependencyApi check to support scoped apis

--- ern-core/src/utils.js
+++ ern-core/src/utils.js
@@ -75,11 +75,11 @@ export function isDependencyApiOrApiImpl (dependencyName: string): boolean {
 }
 
 export function isDependencyApi (dependencyName: string): boolean {
-  return (/^react-native-.+-api$/.test(dependencyName))
+  return (/^.*react-native-.+-api$/.test(dependencyName))
 }
 
 export function isDependencyApiImpl (dependencyName: string): boolean {
-  return (/^react-native-.+-api-impl$/.test(dependencyName))
+  return (/^.*react-native-.+-api-impl$/.test(dependencyName))
 }
 
 /**
