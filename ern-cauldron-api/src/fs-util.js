2205f60b7e9da3b595f8d2083d1ce3b8d3c7f2bc
Benoit Lemaire <blemaire@walmartlabs.com>
2017-11-01 23:10:10 -0700

    :arrow_up: Upgrade flow version

--- ern-cauldron-api/src/fs-util.js
+++ ern-cauldron-api/src/fs-util.js
@@ -4,28 +4,26 @@ import fs from 'fs'
 
 export function writeJSON (
   filename: string,
-  json: Object = {}) {
+  json: Object = {}) : Promise<void> {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filename, JSON.stringify(json, null, 2), (e, o) => e ? reject(e) : resolve(o))
+    fs.writeFile(filename, JSON.stringify(json, null, 2), e => e ? reject(e) : resolve())
   })
 }
 
 export function writeFile (
   filename: string,
   data: any,
-  options: any) {
+  options: any) : Promise<void> {
   if (!data) {
     data = options
     options = {}
   }
   return new Promise((resolve, reject) => {
-    fs.writeFile(filename, data, (e, o) => {
-      e ? reject(e) : resolve(o)
-    })
+    fs.writeFile(filename, data, e => e ? reject(e) : resolve())
   })
 }
 
-export function readJSON (f: string) {
+export function readJSON (f: string) : Promise<Object> {
   return new Promise((resolve, reject) => {
     fs.readFile(f, function (e, o) {
       if (e) return reject(e)
