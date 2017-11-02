dbb5d47196d0a4fbd1f472496771b729d23e70a6
Benoit Lemaire <blemaire@walmartlabs.com>
2017-11-02 16:35:30 -0700

    Fix coverage script

--- ern-util-dev/bin/ern-nyc.js
+++ ern-util-dev/bin/ern-nyc.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 var fs = require('fs')
+
+process.env['__ERN_TEST__'] = true
+
 if (!fs.existsSync('test')) {
   console.log(`No tests found in ${process.cwd()}/test`)
   process.exit(0)
 }
-process.argv.push('--sourceMap=false', '--reportDir=.coverage', '--instrument=false', '--all', '--include=src/**/*.js', 'mocha', '--compilers', `js:${__dirname}/../babelhook-coverage`, 'test/*-test.js')
+
+process.argv.push(
+  '--sourceMap=false',
+  '--reportDir=.coverage',
+  '--reporter=json',
+  '--reporter=text',
+  '--show-process-tree',
+  '--instrument=false',
+  '--all',
+  `--require=${__dirname}/../babelhook-coverage`,
+  '--only="src/"',
+  'mocha',
+  'test/*-test.js')
+
 require('nyc/bin/nyc')
