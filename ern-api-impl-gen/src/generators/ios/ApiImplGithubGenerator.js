45c5139c3081f3808b554525127dca7d3956f32d
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-11-06 10:58:34 -0800

    add support for generating request handler implementation for ios [api-impl-gen].

--- ern-api-impl-gen/src/generators/ios/ApiImplGithubGenerator.js
+++ ern-api-impl-gen/src/generators/ios/ApiImplGithubGenerator.js
@@ -1,22 +1,26 @@
-
 import {
-  IosUtil
+  IosUtil,
+  Platform
 } from 'ern-core'
 
 import {
   Dependency,
-  shell
+  shell,
+  mustacheUtils
 } from 'ern-util'
 
 import ApiImplGeneratable from '../../ApiImplGeneratable'
 import fs from 'fs'
 import path from 'path'
 import xcode from 'xcode-ern'
+import readDir from 'fs-readdir-recursive'
 
 export const ROOT_DIR = shell.pwd()
+const API_IMPL_GROUP_NAME = 'APIImpls'
 
 export default class ApiImplGithubGenerator implements ApiImplGeneratable {
-  get name () : string {
+  regenerateApiImpl:boolean
+  get name (): string {
     return 'ApiImplGithubGenerator'
   }
 
@@ -25,18 +29,20 @@ export default class ApiImplGithubGenerator implements ApiImplGeneratable {
   }
 
   async generate (apiDependency: Dependency,
-                  paths : Object,
+                  paths: Object,
                   reactNativeVersion: string,
-                  plugins : Array<Dependency>,
+                  plugins: Array<Dependency>,
                   apis: Array<Object>,
                   regen: boolean) {
     log.debug(`Starting project generation for ${this.platform}`)
-    await this.fillHull(paths, reactNativeVersion, plugins)
+    this.regenerateApiImpl = regen
+    await this.fillHull(paths, reactNativeVersion, plugins, apis)
   }
 
   async fillHull (paths: Object,
                   reactNativeVersion: string,
-                  plugins: Array<Dependency>) {
+                  plugins: Array<Dependency>,
+                  apis) {
     try {
       const pathSpec = {
         rootDir: ROOT_DIR,
@@ -52,6 +58,8 @@ export default class ApiImplGithubGenerator implements ApiImplGeneratable {
       this.injectReactNativeToPlugins(reactNativeVersion, plugins)
 
       const {iosProject, projectPath} = await IosUtil.fillProjectHull(pathSpec, projectSpec, plugins)
+
+      await this.generateRequestHandlerClasses(iosProject, pathSpec, projectSpec, apis)
 
       fs.writeFileSync(projectPath, iosProject.writeSync())
 
@@ -71,17 +79,7 @@ export default class ApiImplGithubGenerator implements ApiImplGeneratable {
     plugins.push(reactNativePlugin)
   }
 
-  // Code to keep backward compatibility
-  switchToOldDirectoryStructure (pluginSourcePath: string, tail: string): boolean {
-    // This is to check if the api referenced during container generation is created using the old or new directory structure to help keep the backward compatibility.
-    const pathToSwaggersAPIs = path.join('IOS', 'IOS', 'Classes', 'SwaggersAPIs')
-    if (path.dirname(tail) === `IOS` && fs.existsSync(path.join(pluginSourcePath, path.dirname(pathToSwaggersAPIs)))) {
-      return true
-    }
-    return false
-  }
-
-  async getIosApiImplProject (apiImplProjectPath: string) : Promise<*> {
+  async getIosApiImplProject (apiImplProjectPath: string): Promise<*> {
     log.debug(apiImplProjectPath)
 
     const containerProject = xcode.project(apiImplProjectPath)
@@ -94,5 +92,67 @@ export default class ApiImplGithubGenerator implements ApiImplGeneratable {
         resolve(containerProject)
       })
     })
+  }
+
+  async generateRequestHandlerClasses (iosProject, pathSpec, projectSpec, apis) {
+    log.debug('=== updating request handler implementation class ===')
+
+    const {outputDir, resourceDir} = this.createImplDirectoryAndCopyCommonClasses(pathSpec, projectSpec, iosProject)
+    let editableFiles = []
+    for (const api of apis) {
+      const {files, classNames} = ApiImplGithubGenerator.getMustacheFileNamesMap(resourceDir, api.apiName)
+
+      for (const file of files) {
+        if (!classNames[file]) {
+          log.warn(`Skipping mustaching of ${file}. No resulting file mapping found, consider adding one. \nThis might cause issues in generated implementation project.`)
+          throw new Error(`Class name mapping is missing for ${file}, unable to generate implementation class file.`)
+        }
+
+        if (file === 'requestHandlerProvider.mustache') {
+          editableFiles.push(path.join(outputDir, classNames[file]))
+          if (this.regenerateApiImpl) {
+            log.debug(`Skipping regeneration of ${classNames[file]}`)
+            continue
+          }
+        }
+
+        await mustacheUtils.mustacheRenderToOutputFileUsingTemplateFile(
+          path.join(resourceDir, file),
+          api,
+          path.join(outputDir, classNames[file]))
+        iosProject.addSourceFile(path.join(API_IMPL_GROUP_NAME, classNames[file]), null, iosProject.findPBXGroupKey({name: API_IMPL_GROUP_NAME}))
+      }
+
+      log.debug(`Api implementation files successfully generated for ${api.apiName}Api`)
+    }
+  }
+
+  static getMustacheFileNamesMap (resourceDir, apiName) {
+    const files = readDir(resourceDir, (f) => (f.endsWith('.mustache')))
+    const classNames = {
+      'requestHandlerDelegate.mustache': `${apiName}ApiRequestHandlerDelegate.swift`,
+      'requestHandlerProvider.mustache': `${apiName}ApiRequestHandlerProvider.swift`,
+      'apiController.mustache': `${apiName}ApiController.swift`
+    }
+    return {files, classNames}
+  }
+
+  createImplDirectoryAndCopyCommonClasses (pathSpec, projectSpec, iosProject) {
+    const resourceDir = path.join(Platform.currentPlatformVersionPath, 'ern-api-impl-gen', 'resources', 'ios')
+    const outputDir = path.join(pathSpec.outputDir, projectSpec.projectName, API_IMPL_GROUP_NAME)
+
+    const requestHandlerConfigFile = 'RequestHandlerConfig.swift'
+    const requestHandlerProviderFile = 'RequestHandlerProvider.swift'
+
+    if (!shell.test('-e', outputDir)) {
+      shell.mkdir(outputDir)
+    }
+    shell.cp(path.join(resourceDir, requestHandlerConfigFile), outputDir)
+    shell.cp(path.join(resourceDir, requestHandlerProviderFile), outputDir)
+
+    iosProject.addSourceFile(path.join(API_IMPL_GROUP_NAME, requestHandlerConfigFile), null, iosProject.findPBXGroupKey({name: API_IMPL_GROUP_NAME}))
+    iosProject.addSourceFile(path.join(API_IMPL_GROUP_NAME, requestHandlerProviderFile), null, iosProject.findPBXGroupKey({name: API_IMPL_GROUP_NAME}))
+
+    return {outputDir, resourceDir}
   }
 }
