b4cf5ba580d4ab097ed3e7523b4de74e88cd9649
Cory Reed <swashcap@gmail.com>
2017-11-07 15:23:51 -0800

    style(ern-util): correct log misspelling
    [skip ci]

--- ern-util/src/utils.js
+++ ern-util/src/utils.js
@@ -1,6 +1,6 @@
 export default class Utils {
   static logErrorAndExitProcess (e: Error, code?: number = 1) {
-    log.error(`An error hapenned : ${e.message}`)
+    log.error(`An error occurred: ${e.message}`)
     log.debug(e)
     process.exit(code)
   }
