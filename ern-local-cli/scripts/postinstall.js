ae9235a9102d1918e05434ac2697099df9736212
Benoit Lemaire <blemaire@walmartlabs.com>
2017-11-07 11:39:33 -0800

    v0.10.1

--- ern-local-cli/scripts/postinstall.js
+++ ern-local-cli/scripts/postinstall.js
@@ -16,7 +16,7 @@ const os = require('os')
 // ....
 // |_ .ernrc
 
-const PLATFORM_VERSION = '0.10.0'
+const PLATFORM_VERSION = '0.10.1'
 // Path to ern platform root directory
 const ERN_PATH = process.env['ERN_HOME'] || path.join(os.homedir(), '.ern')
 // Path to ern global configuration file
