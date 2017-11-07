2be6b8fc5c86640ab1c0c3c8236aaaa098a5bef4
krunalsshah <krunal.shah.neu@gmail.com>
2017-11-06 17:02:06 -0800

    Remove unused configFilePath from normalizeConfig

--- ern-api-gen/src/Constants.js
+++ ern-api-gen/src/Constants.js
@@ -1,12 +1,10 @@
 // @flow
 
-export const CONFIG_FILE = 'apigen.conf.json'
 export const MODEL_FILE = 'schema.json'
 export const PKG_FILE = 'package.json'
 export const FLOW_CONFIG_FILE = ".flowconfig"
 export const FLOW_BIN_VERSION = "^0.47.0"
 export default ({
-  CONFIG_FILE,
   MODEL_FILE,
   PKG_FILE,
   FLOW_CONFIG_FILE,
