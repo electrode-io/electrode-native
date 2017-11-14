79e2e074668361cd73bfaeb42d73ddc170e75a89
krunalsshah <krunal.shah.neu@gmail.com>
2017-11-13 18:53:52 -0800

    test android ern-util

---
+++ ern-util/test/fixtures/common.js
@@ -0,0 +1,22 @@
+export const deviceOne = 'emulator-5554	device'
+
+export const deviceTwo = '8XV7N16516003608	device'
+
+export const multipleAvdList = [
+  'Nexus6API23M',
+  'Nexus_5X_API_24'
+]
+
+export const oneAvdList = [
+  'Nexus6API23M'
+]
+
+export const oneAvd = 'Nexus6API23M'
+
+export const pkgName ='chai'
+
+export const activityName = 'ChaiActivity'
+
+export const pkgPath = 'pkgPath'
+
+
