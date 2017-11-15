60428b694041deb17d216c7e0c533695d0f0c79f
krunalsshah <krunal.shah.neu@gmail.com>
2017-11-15 01:25:13 -0800

    add unit test for required

---
+++ ern-util/test/required-test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import required from '../src/required'
+
+describe('required', () => {
+  it('test required', () => {
+      try{
+        required('param1')
+      } catch (e){
+        expect(e.message).to.eql('param1 is required')
+      }
+  })
+})
