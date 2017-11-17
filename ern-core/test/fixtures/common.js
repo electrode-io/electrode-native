bd18fa2cd69181717aabb34faed1660bb09f7217
krunalsshah <krunal.shah.neu@gmail.com>
2017-11-16 18:58:56 -0800

    add core util sync tests , fix isPublishedToNpm bug

---
+++ ern-core/test/fixtures/common.js
@@ -0,0 +1,7 @@
+export const pkgNameNotInNpm = 'zxc-pkg-not-in-npm-bnm'
+export const pkgName = 'chai'
+export const pkgNameWithVersion = 'chai@4.1.2'
+export const pkgNameWithInvalidVersion = 'chai@1000.1000.0'
+export const moduleTypeNotSupported = 'moduleTypeNotSupported'
+
+
