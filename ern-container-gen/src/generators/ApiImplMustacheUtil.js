46c77904bd12d18b16c1ad22f61b42263f332e0a
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-11-21 16:43:51 -0800

    [container-gen-native] generated container should not include native code inside ElectrodeContainer initialize.

--- ern-container-gen/src/generators/ApiImplMustacheUtil.js
+++ ern-container-gen/src/generators/ApiImplMustacheUtil.js
@@ -1,5 +1,6 @@
 import {
-  utils
+  utils,
+  ModuleTypes
 } from 'ern-core'
 import fs from 'fs'
 import path from 'path'
@@ -8,13 +9,21 @@ import path from 'path'
  *
  * @param apiImplPluginPath : node package directory from for the api impl module.
  * @param mustacheView: Object
+ * @param excludeJsImpl: setting this to true will exclude api details for a JS implementation.
+ * @param excludeNativeImpl: setting this to true will exclude api details for a native implementation.
  */
-export default function populateApiImplMustacheView (apiImplPluginPath: string, mustacheView?: Object = {}) {
+export default function populateApiImplMustacheView (apiImplPluginPath: string, mustacheView?: Object = {}, excludeJsImpl?: boolean, excludeNativeImpl?: boolean) {
   const packageJson = JSON.parse(fs.readFileSync(path.join(apiImplPluginPath, 'package.json'), 'utf-8'))
   const containerGenConfig = packageJson.ern.containerGen
   if (containerGenConfig && containerGenConfig.apiNames) {
     mustacheView.apiImplementations = mustacheView.apiImplementations ? mustacheView.apiImplementations : []
     for (const apiName of containerGenConfig.apiNames) {
+      if (excludeJsImpl && packageJson.ern.moduleType === ModuleTypes.JS_API_IMPL) {
+        continue
+      }
+      if (excludeNativeImpl && packageJson.ern.moduleType === ModuleTypes.NATIVE_API_IMPL) {
+        continue
+      }
       let api = {
         apiName,
         hasConfig: containerGenConfig.hasConfig,
