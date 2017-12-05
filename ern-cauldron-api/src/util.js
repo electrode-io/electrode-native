b8129c34d13aaf7873f506673e544f3fbc6a4cda
Benoit Lemaire <blemaire@walmartlabs.com>
2017-12-05 10:34:12 -0800

    :white_check_mark: Add tests for CauldronApi utils

--- ern-cauldron-api/src/util.js
+++ ern-cauldron-api/src/util.js
@@ -32,45 +32,6 @@ export function getNativeBinaryFileExt (platformName: string) {
   return platformName === 'android' ? 'apk' : 'app'
 }
 
-export function buildReactNativeSourceMapFileName (
-  appName: string,
-  versionName: string) {
-  return `${appName}@${versionName}.map`
-}
-
-//
-// Removes the version from a dependency string and return the new string
-// Sample inputs => outputs :
-// react-native@0.42.0 => react-native
-// @walmart/react-something@1.0.0 => @walmart/react-something
-export function removeVersionFromDependency (dependency: string) {
-  return /^(@?.+)@.+$/.exec(dependency)[1]
-}
-
-//
-// Check if a dependency is present in a given array of dependencies
-// nativeDependencies : An array containing the native dependencies as strings
-//   Sample : [ "react-native-electrode-bridge@1.0.0" , "react-native@0.42.0" ]
-//
-// dependency : A dependency string
-//   Sample : "react-native@0.42.0"
-//
-// shouldMatchVersion : if set to true, the version of the dependency has to match
-//                      for it to be considered present.
-export function containsDependency (
-  nativeDependencies: Array<string>,
-  dependency: string, {
-    shouldMatchVersion
-  } : {
-    shouldMatchVersion?: boolean
-  } = {}) {
-  if (!shouldMatchVersion) {
-    dependency = `${removeVersionFromDependency(dependency)}@`
-  }
-
-  return _.some(nativeDependencies, d => d.startsWith(dependency))
-}
-
 export function joiValidate (payload: any, schema: any) : Promise<any> {
   return new Promise(function (resolve, reject) {
     Joi.validate(payload, schema, (err, value) => {
