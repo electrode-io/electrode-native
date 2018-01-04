bc55961a614203702abb52a0fe49dce7283b52dc
Benoit Lemaire <blemaire@walmartlabs.com>
2018-01-04 10:26:55 -0800

    :bug: Fix a bug in plugins view creation during container generation

--- ern-container-gen/src/utils.js
+++ ern-container-gen/src/utils.js
@@ -389,17 +389,24 @@ export async function generatePluginsMustacheViews (
       log.warn(`${plugin.name} does not have any injection configuration for ${platform} platform`)
       continue
     }
-    let pluginView = {}
     const pluginHook = pluginConfig[platform].pluginHook
+    const containerHeader = pluginConfig[platform].containerPublicHeader
+
+    if (!pluginHook && !containerHeader) {
+      continue
+    }
+
+    let pluginView = {}
     if (pluginHook) {
       pluginView.name = pluginHook.name
       pluginView.lcname = pluginHook.name && pluginHook.name.charAt(0).toLowerCase() + pluginHook.name.slice(1)
       pluginView.configurable = pluginHook.configurable
     }
-    let containerHeader = pluginConfig[platform].containerPublicHeader
+
     if (containerHeader) {
       pluginView.containerHeader = containerHeader
     }
+
     pluginsViews.push(pluginView)
   }
   return pluginsViews
