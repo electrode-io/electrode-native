c64a6460ad7a4cdb90629d46783b931223d03fac
Benoit Lemaire <blemaire@walmartlabs.com>
2018-01-24 10:56:35 -0800

    Fix cauldron upgrade command

--- ern-local-cli/src/commands/cauldron/upgrade.js
+++ ern-local-cli/src/commands/cauldron/upgrade.js
@@ -9,13 +9,11 @@ exports.command = 'upgrade'
 exports.desc = 'Upgrade the Cauldron schema'
 exports.builder = function (yargs: any) {}
 exports.handler = async function () {
-  await utils.logErrorAndExitIfNotSatisfied({
-    cauldronIsActive: {
-      extraErrorMessage: 'A Cauldron must be active in order to use this command'
-    }
-  })
   try {
     const cauldron = await coreUtils.getCauldronInstance({ignoreSchemaVersionMismatch: true})
+    if (!cauldron) {
+      throw new Error('A Cauldron must be active in order to use this command')
+    }
     await cauldron.upgradeCauldronSchema()
     log.debug(`Cauldron was succesfully upgraded !`)
   } catch (e) {
