c4eab02f6ab3e3cbf62bed046d074d33b56c2b1d
Benoit Lemaire <blemaire@walmartlabs.com>
2018-02-12 13:47:11 -0800

    Add containsVersionMismatch function to publication lib

--- ern-local-cli/test/publication-test.js
+++ ern-local-cli/test/publication-test.js
@@ -96,4 +96,62 @@ describe('lib/publication.js', () => {
       expect(getCodePushSdk).to.throw()
     })
   })
+
+  // ==========================================================
+  // containsVersionMismatch
+  // ==========================================================
+  const versionsWithAMajorMismatch = [ '1.0.0', '2.0.0', '1.0.0' ]
+  const versionsWithAMinorMismatch = [ '1.0.0', '1.1.0', '1.0.0' ]
+  const versionsWithAPatchMismatch = [ '1.0.0', '1.0.1', '1.0.0' ]
+  const versionsWithoutMismatch = [ '1.0.0', '1.0.0', '1.0.0' ]
+
+  describe('containsVersionMismatch', () => {
+    it('should return true if mismatch level is set to major and there is at least one major version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAMajorMismatch, 'major')).true
+    })
+
+    it('should return false if mismatch level is set to major and there is no major version mismatch [1]', () => {
+      expect(containsVersionMismatch(versionsWithAMinorMismatch, 'major')).false
+    })
+
+    it('should return false if mismatch level is set to major and there is no major version mismatch [2]', () => {
+      expect(containsVersionMismatch(versionsWithAPatchMismatch, 'major')).false
+    })
+
+    it('should return false if mismatch level is set to major and there is no major version mismatch [3]', () => {
+      expect(containsVersionMismatch(versionsWithoutMismatch, 'major')).false
+    })
+
+    it('should return true if mismatch level is set to minor and there is at least one major version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAMajorMismatch, 'minor')).true
+    })
+
+    it('should return true if mismatch level is set to minor and there is at least one minor version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAMinorMismatch, 'minor')).true
+    })
+
+    it('should return false if mismatch level is set to minor and there no minor version mismatch [1]', () => {
+      expect(containsVersionMismatch(versionsWithAPatchMismatch, 'minor')).false
+    })
+
+    it('should return false if mismatch level is set to minor and there no minor version mismatch [1]', () => {
+      expect(containsVersionMismatch(versionsWithoutMismatch, 'minor')).false
+    })
+
+    it('should return true if mismatch level is set to patch and there is at least one major version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAMajorMismatch, 'patch')).true
+    })
+
+    it('should return true if mismatch level is set to patch and there is at least one minor version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAMinorMismatch, 'patch')).true
+    })
+
+    it('should return true if mismatch level is set to patch and there is at least one patch version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithAPatchMismatch, 'patch')).true
+    })
+
+    it('should return false if mismatch level is set to patch and there is no patch version mismatch', () => {
+      expect(containsVersionMismatch(versionsWithoutMismatch, 'patch')).false
+    })
+  })
 })
