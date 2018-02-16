12561dc29588f2166f78a80682892bea73869c4d
Benoit Lemaire <lemaireb@gmail.com>
2018-02-15 17:26:12 -0800

    :bug: Fix some android adb related functions on Windows

--- ern-core/test/fixtures/common.js
+++ ern-core/test/fixtures/common.js
@@ -1,3 +1,4 @@
+import os from 'os'
 export const pkgNameNotInNpm = 'zxc-pkg-not-in-npm-bnm'
 export const pkgName = 'chai'
 export const pkgNameWithVersion = 'chai@4.1.2'
@@ -10,7 +11,7 @@ export const oneAvdList = ['Nexus6API23M']
 export const oneAvd = 'Nexus6API23M'
 export const activityName = 'ChaiActivity'
 export const projectPath = 'projectPath'
-export const getDeviceResult = 'emulator-5554\tdevice\n8XV7N16516003608\tdevice'
+export const getDeviceResult = `emulator-5554\tdevice${os.EOL}8XV7N16516003608\tdevice`
 export const oneUdid = 'A1213FE6-BDA8-424B-972C-4EA0480C3497'
 
 export const validElectrodeNativeModuleNames = [
