d0d3dc96ed90e6ea9b51cf3384b3199fe4853dd7
Benoit Lemaire <lemaireb@gmail.com>
2018-03-02 14:01:48 -0800

    :hammer: Allow for granular execution of system test suites

--- system-tests/system-tests.js
+++ system-tests/system-tests.js
@@ -1,13 +1,39 @@
 const fs = require('fs')
 const path = require('path')
 const tmp = require('tmp')
+const inquirer = require('inquirer');
+const run = require('./utils/run')
+
+const runAll = process.argv.includes('--all')
 
 const pathToSystemTests = path.join(__dirname, 'tests')
-const workingDirPath = tmp.dirSync({ unsafeCleanup: true }).name
-process.chdir(workingDirPath)
+const testsSourceFiles = fs.readdirSync(pathToSystemTests)
 
-const tests = fs.readdirSync(pathToSystemTests)
-for (const test of tests) {
-  console.log(`Running ${test} tests`)
-  require(path.join(pathToSystemTests, test))
+if (runAll) {
+  runAllTests()
+} else {
+  inquirer.prompt([{
+    type: 'checkbox',
+    name: 'userSelectedTests',
+    message: 'Choose one or more tests to run',
+    choices: testsSourceFiles
+  }]).then(answers => {
+    for (const userSelectedTestSourceFile of answers.userSelectedTests) {
+      runTest(userSelectedTestSourceFile)
+    }
+  })
+}
+
+function runAllTests() {
+  for (const testSourceFile of testsSourceFiles) {
+    runTest(testSourceFile)
+  }
+}
+
+function runTest(testSourceFile) {
+  run(`ern cauldron repo clear`)
+  const workingDirPath = tmp.dirSync({ unsafeCleanup: true }).name
+  process.chdir(workingDirPath)
+  console.log(`Running ${testSourceFile} tests`)
+  require(path.join(pathToSystemTests, testSourceFile))
 }
