6aa9f68ee814599b30e6d52bf44daac964b1d700
Benoit Lemaire <blemaire@walmartlabs.com>
2018-04-23 17:23:27 -0700

    :white_check_mark: Regen system tests API fixtures

--- system-tests/fixtures/api-impl-native/ReactNativeErnmovieApiImplNative/ios/ElectrodeApiImpl/Libraries/ReactNative/Image/resolveAssetSource.js
+++ system-tests/fixtures/api-impl-native/ReactNativeErnmovieApiImplNative/ios/ElectrodeApiImpl/Libraries/ReactNative/Image/resolveAssetSource.js
@@ -1,10 +1,8 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  * @providesModule resolveAssetSource
  * @flow
@@ -94,7 +92,7 @@ if (!sourceCode) {
   const NativeModules = require('NativeModules');
   sourceCode = NativeModules && NativeModules.SourceCode;
 }
-_sourceCodeScriptURL = sourceCode.scriptURL;
+_sourceCodeScriptURL = sourceCode && sourceCode.scriptURL;
 
 module.exports = resolveAssetSource;
 module.exports.pickScale = AssetSourceResolver.pickScale;
