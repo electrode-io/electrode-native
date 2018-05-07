91dd7e61e4ec44eb4cc5d3670976912c74b250b4
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-07 12:32:02 -0700

    :white_check_mark: Update iOS Container system test fixture

--- system-tests/fixtures/ios-container/ElectrodeContainer/ElectrodeBridgeDelegate.h
+++ system-tests/fixtures/ios-container/ElectrodeContainer/ElectrodeBridgeDelegate.h
@@ -23,45 +23,14 @@
 #else
 #import "React/RCTBridgeDelegate.h"   // Required when used as a Pod in a Swift project
 #endif
+#import "ElectrodePluginConfig.h"
+
 NS_ASSUME_NONNULL_BEGIN
-////////////////////////////////////////////////////////////////////////////////
-#pragma mark - ElectrodePluginConfigurator
-/**
- Used as configuration for the start up of the ElectrodeReactNative system. Build
- a class that adheres to this
- */
-@protocol ElectrodePluginConfig <NSObject>
--(void)setupConfigWithDelegate: (id<RCTBridgeDelegate>) delegate;
-
-@optional
-// Optional Instance Methods
-
-/**
- Builds an instance of the configurator based off of a plist of configuration.
- 
- @param plist A string of the name of the plist with configuration in it.
- @return instancetype of the class that adheres to the protocol.
- */
-- (instancetype)initWithIsDebugEnabled: (BOOL) enabled;
-- (instancetype)initWithPlist:(NSString *)plist;
-- (instancetype)initWithDeploymentKey: (NSString *)deploymentKey;
-
-// Optional Properties
-@property (nonatomic, copy, readonly) NSString *codePushWithServerURLString;
-
-@property (nonatomic, copy, readonly) NSString *codePushWithIDString;
-
-@end
-
 
 @interface ElectrodeBridgeDelegate : NSObject <RCTBridgeDelegate>
 @property (nonatomic, strong) NSURL *jsBundleURL;
 
 - (instancetype)initWithModuleURL:(NSURL *)url extraModules:(NSArray *)modules;
-/*
- * @deprecate
- */
-- (instancetype)initWithURL: (NSURL *)url;
 
 - (instancetype)initWithContainerConfig: (id<ElectrodePluginConfig>) containerConfig
                          codePushConfig: (id<ElectrodePluginConfig>) codePushConfig;
