123db95ae332aa988ab6ae93b2bc0f7a09134871
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-09 15:16:08 -0700

    :wrench: Remove dist directory in ern-typescript script

--- ern-util-dev/bin/ern-typescript.js
+++ ern-util-dev/bin/ern-typescript.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-const ernModulePath = process.cwd()
 const path = require('path')
+const ernModulePath = process.cwd()
+require('rimraf').sync(path.resolve(ernModulePath, 'dist'))
 process.argv.push('--project')
 process.argv.push(path.resolve(ernModulePath, 'tsconfig.release.json'))
 console.log('running tsc with', process.argv.slice(2))
