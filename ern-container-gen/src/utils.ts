c8fa99ecb4a52dc089d607cbb03597e96ad993fc
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-09 11:38:43 -0700

    :hammer: Remove duplicated code in Container generators

--- ern-container-gen/src/utils.ts
+++ ern-container-gen/src/utils.ts
@@ -660,6 +660,26 @@ export function populateApiImplMustacheView(
   }
 }
 
+export function prepareDirectories(conf: ContainerGeneratorConfig) {
+  if (!fs.existsSync(conf.outDir)) {
+    shell.mkdir('-p', conf.outDir)
+  } else {
+    shell.rm('-rf', path.join(conf.outDir, '{.*,*}'))
+  }
+
+  if (!fs.existsSync(conf.compositeMiniAppDir)) {
+    shell.mkdir('-p', conf.compositeMiniAppDir)
+  } else {
+    shell.rm('-rf', path.join(conf.compositeMiniAppDir, '{.*,*}'))
+  }
+
+  if (!fs.existsSync(conf.pluginsDownloadDir)) {
+    shell.mkdir('-p', conf.pluginsDownloadDir)
+  } else {
+    shell.rm('-rf', path.join(conf.pluginsDownloadDir, '{.*,*}'))
+  }
+}
+
 export async function addElectrodeNativeMetadataFile(
   conf: ContainerGeneratorConfig
 ) {
