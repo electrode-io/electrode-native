084b103e0bef50f447bf8196d488359b23117945
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-15 11:06:25 -0700

    :wrench: Update auto-rebuild script for yarn workspace

--- auto-rebuild.js
+++ auto-rebuild.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 var execSync = require('child_process').execSync
 
-var stdout = execSync('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD')
+var stdout = execSync(
+  'git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD'
+)
 
 if (stdout) {
   if (/\nyarn\.lock/.test(stdout)) {
     console.log('========================================================')
-    console.log('Dependency changes detected in Electrode Native root')
+    console.log('Dependency changes detected')
     console.log('Running yarn install')
     console.log('========================================================')
     execSync('yarn install', { stdio: [0, 1, 2] })
-  }
-  if (/ern-.+(\/|\\)yarn\.lock/.test(stdout)) {
-    console.log('========================================================')
-    console.log('Dependency changes detected in one or more ern module(s)')
-    console.log('Rebuilding Electrode Native platform')
-    console.log('========================================================')
-    execSync('npm run rebuild', { stdio: [0, 1, 2] })
   }
 }
