8f1ed98554a2e06b132a2028a2755ea78c889ffe
Benoit Lemaire <lemaireb@gmail.com>
2018-05-20 10:21:22 -0700

    :wrench: Fix TypeScript compilation on Windows

--- ern-util-dev/bin/ern-mocha.js
+++ ern-util-dev/bin/ern-mocha.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
-process.env['__ERN_TEST__'] = true
+const path = require('path')
+process.env.__ERN_TEST__ = true
+process.env.TS_NODE_PROJECT = path.resolve('..', 'tsconfig.json')
 if (!require('fs').existsSync(require('path').join(process.cwd(), 'test'))) {
   console.log('no tests for project ', process.cwd())
   process.exit(0)
 }
 console.log(`running tests in ${process.cwd()}`)
-process.argv.push('-r', 'tsconfig-paths/register')
 process.argv.push('-r', 'ts-node/register')
+process.argv.push('-r', 'tsconfig-paths/register')
 process.argv.push('test/*-test.{ts,js}')
 require('mocha/bin/_mocha')
