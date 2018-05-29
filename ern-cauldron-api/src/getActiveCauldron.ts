e54f6ec642396b97fc2bf3f5631b11e9e11553ba
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-29 13:27:41 -0700

    Improve getActiveCauldron function

--- ern-cauldron-api/src/getActiveCauldron.ts
+++ ern-cauldron-api/src/getActiveCauldron.ts
@@ -9,54 +9,56 @@ import path from 'path'
 // Returns undefined if no Cauldron is active
 // Throw error if Cauldron is not using the correct schema version
 let currentCauldronHelperInstance
+let currentCauldronRepoInUse
+
 export default async function getActiveCauldron({
   ignoreSchemaVersionMismatch,
 }: {
   ignoreSchemaVersionMismatch?: boolean
 } = {}): Promise<CauldronHelper> {
-  if (!currentCauldronHelperInstance) {
+  const repoInUse = config.getValue('cauldronRepoInUse')
+  if (repoInUse && repoInUse !== currentCauldronRepoInUse) {
     const cauldronRepositories = config.getValue('cauldronRepositories')
-    const cauldronRepoInUse = config.getValue('cauldronRepoInUse')
-    if (cauldronRepoInUse) {
-      const cauldronRepoUrl = cauldronRepositories[cauldronRepoInUse]
-      const cauldronRepoBranchReResult = /#(.+)$/.exec(cauldronRepoUrl)
-      const cauldronRepoUrlWithoutBranch = cauldronRepoUrl.replace(/#(.+)$/, '')
-      const cauldronCli = defaultCauldron(
-        cauldronRepoUrlWithoutBranch,
-        path.join(Platform.rootDirectory, 'cauldron'),
-        cauldronRepoBranchReResult ? cauldronRepoBranchReResult[1] : 'master'
-      )
-      currentCauldronHelperInstance = new CauldronHelper(cauldronCli)
-      const schemaVersionUsedByCauldron = await currentCauldronHelperInstance.getCauldronSchemaVersion()
-      const schemaVersionOfCurrentCauldronApi = getCurrentSchemaVersion()
+    const cauldronRepoUrl = cauldronRepositories[repoInUse]
+    const cauldronRepoBranchReResult = /#(.+)$/.exec(cauldronRepoUrl)
+    const cauldronRepoUrlWithoutBranch = cauldronRepoUrl.replace(/#(.+)$/, '')
+    const cauldronCli = defaultCauldron(
+      cauldronRepoUrlWithoutBranch,
+      path.join(Platform.rootDirectory, 'cauldron'),
+      cauldronRepoBranchReResult ? cauldronRepoBranchReResult[1] : 'master'
+    )
+    currentCauldronHelperInstance = new CauldronHelper(cauldronCli)
+    const schemaVersionUsedByCauldron = await currentCauldronHelperInstance.getCauldronSchemaVersion()
+    const schemaVersionOfCurrentCauldronApi = getCurrentSchemaVersion()
+    if (
+      !ignoreSchemaVersionMismatch &&
+      schemaVersionUsedByCauldron !== schemaVersionOfCurrentCauldronApi
+    ) {
       if (
-        !ignoreSchemaVersionMismatch &&
-        schemaVersionUsedByCauldron !== schemaVersionOfCurrentCauldronApi
+        semver.gt(
+          schemaVersionUsedByCauldron,
+          schemaVersionOfCurrentCauldronApi
+        )
       ) {
-        if (
-          semver.gt(
-            schemaVersionUsedByCauldron,
-            schemaVersionOfCurrentCauldronApi
-          )
-        ) {
-          throw new Error(
-            `Cauldron schema version mismatch (${schemaVersionUsedByCauldron} > ${schemaVersionOfCurrentCauldronApi}).
+        throw new Error(
+          `Cauldron schema version mismatch (${schemaVersionUsedByCauldron} > ${schemaVersionOfCurrentCauldronApi}).
 You should switch to a newer platform version that supports this Cauldron schema.`
-          )
-        } else if (
-          semver.lt(
-            schemaVersionUsedByCauldron,
-            schemaVersionOfCurrentCauldronApi
-          )
-        ) {
-          throw new Error(
-            `Cauldron schema version mismatch (${schemaVersionUsedByCauldron} < ${schemaVersionOfCurrentCauldronApi}.
+        )
+      } else if (
+        semver.lt(
+          schemaVersionUsedByCauldron,
+          schemaVersionOfCurrentCauldronApi
+        )
+      ) {
+        throw new Error(
+          `Cauldron schema version mismatch (${schemaVersionUsedByCauldron} < ${schemaVersionOfCurrentCauldronApi}.
 You should run the following command : 'ern cauldron upgrade' to upgrade your Cauldron to the latest version.
 You can also switch to an older version of the platform which supports this Cauldron schema version.`
-          )
-        }
+        )
       }
     }
+    currentCauldronRepoInUse = repoInUse
   }
+
   return Promise.resolve(currentCauldronHelperInstance)
 }
