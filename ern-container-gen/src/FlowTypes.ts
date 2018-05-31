eac087599cd24e701a845fdeaadda1f4c1692b92
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-30 17:59:56 -0700

    :pencil: Reformat some comments to be JSDoc compliant

--- ern-container-gen/src/FlowTypes.ts
+++ ern-container-gen/src/FlowTypes.ts
@@ -1,43 +1,73 @@
 import { PackagePath, MiniApp, BundlingResult } from 'ern-core'
 
 export interface ContainerGeneratorPaths {
-  // Where we assemble the miniapps together
+  /**
+   * Where we assemble the miniapps together
+   */
   compositeMiniApp: string
-  // Where we download plugins
+  /**
+   * Where we download plugins
+   */
   pluginsDownloadDirectory: string
-  // Where we output final generated container
+  /**
+   * Where we output final generated container
+   */
   outDirectory: string
 }
 
 export interface ContainerGeneratorConfig {
-  // The MiniApps that should be included in the generated Container
+  /**
+   * The MiniApps that should be included in the generated Container
+   */
   miniApps: MiniApp[]
-  // The plugins that should be included in the generated Container
+  /**
+   * The plugins that should be included in the generated Container
+   */
   plugins: PackagePath[]
-  // The JS API implementations that should be included in the generated Container
+  /**
+   * The JS API implementations that should be included in the generated Container
+   */
   jsApiImpls: PackagePath[]
-  // The output directory where to generate the Container
+  /**
+   * The output directory where to generate the Container
+   */
   outDir: string
-  // Directory where the plugins should be downloaded to during generation
+  /**
+   * Directory where the plugins should be downloaded to during generation
+   */
   pluginsDownloadDir: string
-  // Directory where the MiniApps will be composed together during generation
+  /**
+   * Directory where the MiniApps will be composed together during generation
+   */
   compositeMiniAppDir: string
-  // Indicates whether rmpm assets should be included in the Container
+  /**
+   * Indicates whether rmpm assets should be included in the Container
+   */
   ignoreRnpmAssets?: boolean
-  // Path to the current yarn lock
+  /**
+   * Path to the current yarn lock
+   */
   pathToYarnLock?: string
 }
 
 export interface ContainerGenResult {
-  // Metadata resulting from the bundling
+  /**
+   *  Metadata resulting from the bundling
+   */
   bundlingResult: BundlingResult
 }
 
 export interface ContainerGenerator {
-  // Name of the Container Generator
+  /**
+   *  Name of the Container Generator
+   */
   readonly name: string
-  // Native platform that this generator targets
+  /**
+   * Native platform that this generator targets
+   */
   readonly platform: string
-  // Generate a Container
+  /**
+   *  Generate a Container
+   */
   generate(config: ContainerGeneratorConfig): Promise<ContainerGenResult>
 }
