2a2b3b88201666cff3bc72d4d7ab9f310d54ad32
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-31 19:17:00 -0700

    Add functions to retrieve Container platform to ern-container-gen utils

--- ern-container-gen/src/index.ts
+++ ern-container-gen/src/index.ts
@@ -1,15 +1,20 @@
-import { generateMiniAppsComposite as _generateMiniAppsComposite } from './utils'
+import {
+  generateMiniAppsComposite as _generateMiniAppsComposite,
+  getContainerPlatform as _getContainerPlatform,
+} from './utils'
 import _IosGenerator from './generators/ios/IosGenerator'
 import _AndroidGenerator from './generators/android/AndroidGenerator'
 
 export const AndroidGenerator = _AndroidGenerator
 export const IosGenerator = _IosGenerator
 export const generateMiniAppsComposite = _generateMiniAppsComposite
+export const getContainerPlatform = _getContainerPlatform
 
 export default {
   AndroidGenerator: _AndroidGenerator,
   IosGenerator: _IosGenerator,
   generateMiniAppsComposite: _generateMiniAppsComposite,
+  getContainerPlatform: _getContainerPlatform,
 }
 
 export {
