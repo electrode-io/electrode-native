3f3170c9a6eb1694ecf7dcbc2e53dbd803bdac9d
Benoit Lemaire <blemaire@walmartlabs.com>
2018-05-31 19:23:26 -0700

    :hammer: Update cauldron add publisher command

--- ern-local-cli/src/commands/cauldron/add/publisher.ts
+++ ern-local-cli/src/commands/cauldron/add/publisher.ts
@@ -1,40 +1,48 @@
 import { NativeApplicationDescriptor, utils as coreUtils, log } from 'ern-core'
 import { getActiveCauldron } from 'ern-cauldron-api'
+import { getPublisher, ContainerPublisher } from 'ern-container-publisher'
 import utils from '../../../lib/utils'
 import { Argv } from 'yargs'
 
 export const command = 'publisher'
-export const desc =
-  'Add a publisher url to publish the generated container/archive.'
+export const desc = 'Add a Container publisher for a native application'
 
 export const builder = (argv: Argv) => {
   return argv
-    .option('mavenUrl', {
-      describe:
-        'Maven url to publish the generated aar file for Android container.',
+    .option('publisher', {
+      alias: 'p',
+      demandOption: true,
+      describe: 'The publisher to add',
       type: 'string',
     })
-    .option('githubUrl', {
-      describe:
-        'Github url to publish the generated framework project(if ios) or library project (if android)',
+    .option('url', {
+      alias: 'u',
+      describe: 'The publication url (format is publisher specific)',
       type: 'string',
     })
     .option('descriptor', {
       alias: 'd',
       describe:
-        'A partial native application descriptor(NativeAppName:platform)',
+        'A partial native application descriptor (NativeAppName:platform)',
+      type: 'string',
+    })
+    .option('config', {
+      alias: 'c',
+      describe: 'Optional extra publisher configuration (as a JSON string)',
       type: 'string',
     })
 }
 
 export const handler = async ({
-  mavenUrl,
-  githubUrl,
+  publisher,
+  url,
   descriptor,
+  config,
 }: {
-  mavenUrl?: string
-  githubUrl?: string
-  descriptor?: string
+  publisher: string
+  descriptor: string
+  url?: string
+  config?: any
 }) => {
   try {
     await utils.logErrorAndExitIfNotSatisfied({
@@ -44,33 +52,38 @@ export const handler = async ({
       },
     })
 
-    let url = ''
-    if (mavenUrl) {
-      url = mavenUrl
-    } else if (githubUrl) {
-      url = githubUrl
-    } else {
-      throw new Error(
-        'Please provide a publisher option (publisher --mavenUrl or --githubUrl)'
-      )
+    // JSON config validation
+    if (config) {
+      try {
+        JSON.parse(config)
+      } catch (e) {
+        throw new Error('config should be valid JSON')
+      }
     }
 
-    log.debug(`Adding ${mavenUrl ? 'maven' : 'gitHub'}publisher: ${url}`)
+    const p: ContainerPublisher = await getPublisher(publisher)
 
     let napDescriptor
     if (descriptor) {
       napDescriptor = NativeApplicationDescriptor.fromString(descriptor)
+      if (!p.platforms.includes(napDescriptor.platform)) {
+        throw new Error(
+          `The ${p.name} publisher does not support ${
+            napDescriptor.platform
+          } platform`
+        )
+      }
     }
 
-    if (mavenUrl && napDescriptor && napDescriptor.platform === 'ios') {
-      throw new Error(
-        'Maven publisher is only supported for android, you selected ios.'
-      )
-    }
-    const publisherType = mavenUrl ? 'maven' : 'github'
     const cauldron = await getActiveCauldron()
-    await cauldron.addPublisher(publisherType, url, napDescriptor)
-    log.info(`${publisherType} publisher was successfully added!`)
+    await cauldron.addPublisher(
+      publisher,
+      p.platforms,
+      napDescriptor,
+      url,
+      config ? JSON.parse(config) : undefined
+    )
+    log.info(`${publisher} publisher was successfully added!`)
   } catch (e) {
     coreUtils.logErrorAndExitProcess(e)
   }
