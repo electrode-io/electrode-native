bf2d517cb36d94a028bf3445cd8dc8113c6de625
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-04 10:41:36 -0700

    Comment index.prod.js

--- ern-local-cli/src/index.prod.js
+++ ern-local-cli/src/index.prod.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+
+/**
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * PLEASE KEEP THIS FILE EXTENSION AS .JS
+ * DO NOT CONVERT TO .TS AS THE GLOBAL-CLI
+ * IS LOOKING FOR index.prod.js FILE
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+
 // tslint:disable-next-line:no-var-requires
 require('../dist/index').default()
