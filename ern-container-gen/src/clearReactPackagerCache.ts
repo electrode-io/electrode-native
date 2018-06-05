58cea19eb32b9c53182cd469d1d9dba5cad1edcb
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-05 13:55:58 -0700

    Use path.join instead of harcoded path in clearReactPackagerCache

--- ern-container-gen/src/clearReactPackagerCache.ts
+++ ern-container-gen/src/clearReactPackagerCache.ts
@@ -1,9 +1,10 @@
 import { shell } from 'ern-core'
+import path from 'path'
 
 // TODO : [WINDOWS SUPPORT]
 export function clearReactPackagerCache() {
   const TMPDIR = process.env.TMPDIR
   if (TMPDIR) {
-    shell.rm('-rf', `${TMPDIR}/react-*`)
+    shell.rm('-rf', path.join(TMPDIR, 'react-*'))
   }
 }
