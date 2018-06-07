0ffcfb443be661b6da62b06ee3d8f086263984f3
krunalsshah <krunal.shah.neu@gmail.com>
2018-06-07 13:08:19 -0700

    block user from passing invalid bridgeVersion

--- ern-local-cli/src/commands/regen-api.ts
+++ ern-local-cli/src/commands/regen-api.ts
@@ -1,5 +1,5 @@
 import { ApiGen } from 'ern-api-gen'
-import { manifest, PackagePath, utils as coreUtils } from 'ern-core'
+import { manifest, PackagePath, utils as coreUtils, yarn } from 'ern-core'
 import utils from '../lib/utils'
 import { Argv } from 'yargs'
 
@@ -26,24 +26,40 @@ export const handler = async ({
   skipVersion: boolean
   bridgeVersion: string
 }) => {
+  const errorMessage =
+    'Run command #yarn info react-native-electrode-bridge versions# to get the valid bridgeVersion'
   try {
-    if (!bridgeVersion) {
-      const bridgeDep = await manifest.getNativeDependency(
-        PackagePath.fromString('react-native-electrode-bridge')
-      )
+    const electrodeBridgePkg = PackagePath.fromString(
+      'react-native-electrode-bridge'
+    )
+    if (bridgeVersion) {
+      const electrodeBridgePkgInfo = await yarn.info(electrodeBridgePkg, {
+        field: 'versions',
+        json: true,
+      })
+      if (
+        electrodeBridgePkgInfo &&
+        electrodeBridgePkgInfo.data &&
+        !electrodeBridgePkgInfo.data.includes(bridgeVersion)
+      ) {
+        throw new Error(
+          `bridgeVersion ${bridgeVersion} is not valid. ${errorMessage}`
+        )
+      }
+    } else {
+      const bridgeDep = await manifest.getNativeDependency(electrodeBridgePkg)
       if (!bridgeDep) {
         throw new Error(
-          'react-native-electrode-bridge not found in manifest. please provide explicit version'
+          `react-native-electrode-bridge is not found in manifest. Specify explicit --bridgeVersion in the command.\n ${errorMessage}`
         )
       }
       if (!bridgeDep.version) {
         throw new Error(
-          'react-native-electrode-bridge version not defined. This should not happen'
+          `react-native-electrode-bridge version not defined. Specify explicit --bridgeVersion in the command. \n ${errorMessage}`
         )
       }
       bridgeVersion = bridgeDep.version
     }
-
     await ApiGen.regenerateCode({ bridgeVersion, skipVersion })
   } catch (e) {
     coreUtils.logErrorAndExitProcess(e)
