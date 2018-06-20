b65590af377c6f96aeb3f848f0fdd6fa672fac39
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-20 15:03:27 -0700

    :package: Introduce ern-runner-gen-android package

---
+++ ern-runner-gen-android/src/index.ts
@@ -0,0 +1,7 @@
+import _AndroidRunnerGenerator from './AndroidRunnerGenerator'
+
+export const AndroidRunnerGenerator = _AndroidRunnerGenerator
+
+export default {
+  AndroidRunnerGenerator: _AndroidRunnerGenerator,
+}
