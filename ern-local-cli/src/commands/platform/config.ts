4c35f795f180ebd1c1aaa1985501b87d605de722
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-21 19:20:45 -0700

    Improve platform config command

--- ern-local-cli/src/commands/platform/config.ts
+++ ern-local-cli/src/commands/platform/config.ts
@@ -1,16 +1,65 @@
 import { config as ernConfig, utils as coreUtils, log } from 'ern-core'
 import utils from '../../lib/utils'
 import { Argv } from 'yargs'
+import levenshtein from 'fast-levenshtein'
 
 export const command = 'config <key> [value]'
 export const desc = 'Get or set a configuration key'
 
 export const builder = (argv: Argv) => {
-  return argv.epilog(utils.epilog(exports))
+  return argv.epilog(userConfigSchemaString() + utils.epilog(exports))
 }
+
+const availableUserConfigKeys = [
+  {
+    desc: 'Set the log level to use for all commands',
+    name: 'logLevel',
+    values: ['trace', 'debug', 'info', 'error', 'fatal'],
+  },
+  {
+    desc: 'Show the Electrode Native banner when running commands',
+    name: 'showBanner',
+    values: [true, false],
+  },
+  {
+    desc: 'Temporary directory to use during commands execution',
+    name: 'tmp-dir',
+    values: ['string'],
+  },
+  {
+    desc: 'Do not remove temporary directories after command execution',
+    name: 'retain-tmp-dir',
+    values: [true, false],
+  },
+]
+
+const userConfigSchemaString = () =>
+  'The following configuration keys are available :\n' +
+  availableUserConfigKeys
+    .map(
+      e =>
+        `${e.name.padEnd(15)} : ${e.desc.padEnd(60)}  [${e.values.join('|')}]`
+    )
+    .join('\n') +
+  '\n\n'
+
+const availableKeys = () => availableUserConfigKeys.map(e => e.name)
+
+const closestKeyName = key =>
+  availableKeys().reduce(
+    (acc, cur) =>
+      levenshtein.get(acc, key) > levenshtein.get(cur, key) ? cur : acc
+  )
 
 export const handler = ({ key, value }: { key: string; value?: string }) => {
   try {
+    if (!availableKeys().includes(key)) {
+      throw new Error(
+        `Configuration key ${key} does not exists. Did you mean ${closestKeyName(
+          key
+        )} ?`
+      )
+    }
     if (value) {
       const valueToset =
         value === 'true' ? true : value === 'false' ? false : value
