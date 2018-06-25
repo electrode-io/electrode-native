98e400ab4494daf60d9810156d6ad370e7423491
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-25 16:38:39 -0700

    Add FsCache class to ern-core

---
+++ ern-core/src/FsCache.ts
@@ -0,0 +1,220 @@
+import fs from 'fs'
+import path from 'path'
+import { readJSON, writeJSON, writeJSONSync } from './fileUtil'
+import shell from './shell'
+import uuidv4 from 'uuid/v4'
+import getSize from 'get-folder-size'
+
+/**
+ * Gets the total size (in bytes) of a given directory
+ * @param dirPath path to the directory for which to compute size
+ */
+const getSizeAsync: (dirPath: string) => Promise<number> = dirPath =>
+  new Promise((resolve, reject) => {
+    getSize(dirPath, (err, size) => {
+      err ? reject(err) : resolve(size)
+    })
+  })
+
+export interface CacheManifest {
+  /**
+   * Entries stored in the cache
+   */
+  entries: CacheManifestEntry[]
+  /**
+   * Total size of the cache
+   */
+  size: number
+}
+
+export interface CacheManifestEntry {
+  /**
+   * Id of the entry
+   * ex: my-miniapp@1.0.0
+   */
+  id: string
+  /**
+   * Absolute path to the entry
+   * ex : /Users/blemair/.ern/cache/78c40fda-d5bb-4681-9bd2-a5fba9df1f70
+   */
+  path: string
+  /**
+   * Size in bytes of the entry
+   */
+  size: number
+  /**
+   * Time (epoc) when the entry was last accessed
+   */
+  lastAccessed: number
+}
+
+const cacheManifestFileName = 'cache-manifest.json'
+export const maxDefaultCacheSize = 2 * 1024 * 1024 * 1024 /* 2GB */
+
+type AddObjectToCacheDirectory<T> = (obj: T, dirPath: string) => Promise<void>
+type ObjectToId<T> = (obj: T) => string
+
+/**
+ * Simple generic file system based LRU cache.
+ * This cache stores arbirtrary objects in unique directories.
+ * The root directory for the cache can be configured, as well as
+ * the maximum size allowed for the cache.
+ * Once cache reaches max size, the least recenctly accessed
+ * objects will be removed from the cache, until size of the cache
+ * falls below maximum size.
+ */
+export class FsCache<T> {
+  /**
+   * Root path to the directory holding the cache
+   */
+  public readonly rootCachePath: string
+  /**
+   * Maximum cache size (in bytes)
+   */
+  public readonly maxCacheSize: number
+  /**
+   * Custom function invoked to add an object entry to the cache
+   */
+  private readonly addObjectToCacheDirectory: AddObjectToCacheDirectory<T>
+  /**
+   * Custom function invoked to convert an object entry to a unique id
+   */
+  private readonly objectToId: ObjectToId<T>
+
+  constructor({
+    /**
+     * Custom function invoked to add an object entry to the cache
+     */
+    addObjectToCacheDirectory,
+    /**
+     * Custom function invoked to convert an object entry to a unique id
+     */
+    objectToId,
+    /**
+     * Maximum cache size (in bytes)
+     */
+    maxCacheSize = maxDefaultCacheSize,
+    /**
+     * Root path to the directory holding the cache
+     */
+    rootCachePath,
+  }: {
+    addObjectToCacheDirectory: AddObjectToCacheDirectory<T>
+    objectToId: ObjectToId<T>
+    maxCacheSize?: number
+    rootCachePath: string
+  }) {
+    if (!addObjectToCacheDirectory) {
+      throw new Error('addObjectToDirectory function is required')
+    }
+    if (!objectToId) {
+      throw new Error('objectToId function is required')
+    }
+    if (!rootCachePath) {
+      throw new Error('rootCachePath is required')
+    }
+    this.rootCachePath = rootCachePath
+    this.maxCacheSize = maxCacheSize
+    this.addObjectToCacheDirectory = addObjectToCacheDirectory
+    this.objectToId = objectToId
+    if (!fs.existsSync(rootCachePath)) {
+      shell.mkdir(rootCachePath)
+    }
+    if (!fs.existsSync(this.cacheManifestPath)) {
+      writeJSONSync(this.cacheManifestPath, { entries: [], size: 0 })
+    }
+  }
+
+  /**
+   * Indicates whether a given object is currently stored in the cache
+   * @param obj The object to check for presence in the cache
+   */
+  public async isInCache(obj: T): Promise<boolean> {
+    const manifestObj = await this.getManifestObj()
+    return !!manifestObj.entries.find(e => e.id === this.objectToId(obj))
+  }
+
+  /**
+   * Adds an object entry to the cache
+   * @param obj The object to add to the cache
+   */
+  public async addToCache(obj: T): Promise<string> {
+    if (await this.isInCache(obj)) {
+      throw new Error(
+        `Object with id ${this.objectToId(obj)} is already in the cache`
+      )
+    }
+    // Create unique directory to store the object
+    const pathToObjectCacheDir = this.createUniqueCacheDirectory()
+    // Invoke client provided function to write object in cache directory
+    await this.addObjectToCacheDirectory(obj, pathToObjectCacheDir)
+    // Compute cached object directory size
+    const cachedObjectDirSize = await getSizeAsync(pathToObjectCacheDir)
+    // Create manifest entry for new cached object
+    const entry: CacheManifestEntry = {
+      id: this.objectToId(obj),
+      lastAccessed: Date.now(),
+      path: pathToObjectCacheDir,
+      size: cachedObjectDirSize,
+    }
+    // Update manifest total cache size
+    const manifestObj = await this.getManifestObj()
+    manifestObj.size += cachedObjectDirSize
+    manifestObj.entries.push(entry)
+    // Clean cache if newly added object caused total cache
+    // size to exceed the defined max cache size
+    // Remove all entries that were least recently accessed
+    // until the total cache size falls below the max cache size
+    const entriesSortedByLat = manifestObj.entries.sort(
+      (a, b) => b.lastAccessed - a.lastAccessed
+    )
+    while (manifestObj.size >= this.maxCacheSize) {
+      const entryToRemove = entriesSortedByLat.pop()
+      manifestObj.size -= entryToRemove!.size
+      shell.rm('-rf', entryToRemove!.id)
+    }
+    manifestObj.entries = entriesSortedByLat
+    // Save updated manifest
+    await writeJSON(this.cacheManifestPath, manifestObj)
+    return pathToObjectCacheDir
+  }
+
+  /**
+   * Gets path to the directory containing a cached object
+   * @param obj The object for which to retrieve cached directory path
+   */
+  public async getObjectCachePath(obj: T): Promise<string | void> {
+    const manifestObj = await this.getManifestObj()
+    const cacheEntry = manifestObj.entries.find(
+      e => e.id === this.objectToId(obj)
+    )
+    if (cacheEntry) {
+      cacheEntry.lastAccessed = Date.now()
+      await writeJSON(this.cacheManifestPath, manifestObj)
+      return cacheEntry.path
+    }
+  }
+
+  /**
+   * Gets the cache manifest object
+   */
+  private async getManifestObj(): Promise<CacheManifest> {
+    return readJSON(this.cacheManifestPath)
+  }
+
+  /**
+   * Creates a uniquely named cache directory
+   */
+  private createUniqueCacheDirectory(): string {
+    const pathToCacheDirectory = path.join(this.rootCachePath, uuidv4())
+    shell.mkdir(pathToCacheDirectory)
+    return pathToCacheDirectory
+  }
+
+  /**
+   * Gets the path to the cache manifest file
+   */
+  private get cacheManifestPath() {
+    return path.join(this.rootCachePath, cacheManifestFileName)
+  }
+}
---
+++ ern-core/test/FsCache-test.ts
@@ -0,0 +1,167 @@
+import { assert, expect } from 'chai'
+import { FsCache, maxDefaultCacheSize } from '../src/FsCache'
+import { writeFile } from '../src/fileUtil'
+import { doesThrow, doesNotThrow } from 'ern-util-dev'
+import shell from '../src/shell'
+import path from 'path'
+import fs from 'fs'
+import _ from 'lodash'
+
+const testRootCachePath = path.join(__dirname, 'tmp')
+
+/**
+ * These tests are using string as the cached object type, for simplicity.
+ * objectToId will return the string itself
+ */
+describe('FsCache', () => {
+  afterEach(() => {
+    shell.rm('-rf', testRootCachePath)
+  })
+
+  describe('constructor', () => {
+    it('should succesfully create a new instance given valid parameters', () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      expect(sut).not.undefined
+    })
+
+    it('should create the root cache directory if it does not exist', () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => '1',
+        rootCachePath: testRootCachePath,
+      })
+      expect(fs.existsSync(testRootCachePath)).true
+    })
+
+    it('should property set the rootCachePath', () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => '1',
+        rootCachePath: testRootCachePath,
+      })
+      expect(sut.rootCachePath).eql(testRootCachePath)
+    })
+
+    it('should use default cache size if not provided', () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => '1',
+        rootCachePath: testRootCachePath,
+      })
+      expect(sut.maxCacheSize).eql(maxDefaultCacheSize)
+    })
+
+    it('should use provided cache size', () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        maxCacheSize: 1000,
+        objectToId: (obj: string) => '1',
+        rootCachePath: testRootCachePath,
+      })
+      expect(sut.maxCacheSize).eql(1000)
+    })
+  })
+
+  describe('isInCache', () => {
+    it('should return false if object is not in cache', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      const isAStringInCache = await sut.isInCache('AString')
+      expect(isAStringInCache).false
+    })
+
+    it('should return true if object is in cache', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      await sut.addToCache('AString')
+      const isAStringInCache = await sut.isInCache('AString')
+      expect(isAStringInCache).true
+    })
+  })
+
+  describe('addToCache', () => {
+    it('should throw if object is already in cache', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      await sut.addToCache('AString')
+      assert(await doesThrow(sut.addToCache, sut, 'AString'))
+    })
+
+    it('should add the object to the cache', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      await sut.addToCache('AString')
+      assert(await sut.isInCache('AString'))
+    })
+
+    it('should remove least recently accessed object from cache once hitting max cache size', async () => {
+      const sut = new FsCache<string>({
+        addObjectToDirectory: async (obj: string, dirPath: string) => {
+          await writeFile(
+            path.join(dirPath, 'filename'),
+            _.fill(Array(500), 0).join('')
+          )
+        },
+        maxCacheSize: 1100,
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      await sut.addToCache('AString')
+      await sut.addToCache('AnotherString')
+      await sut.addToCache('YetAnoterString')
+      assert(await sut.isInCache('YetAnoterString'))
+      assert(!(await sut.isInCache('AnotherString')))
+      assert(!(await sut.isInCache('AString')))
+    })
+  })
+
+  describe('getObjectCachePath', () => {
+    it('should return path to cache directory containing object', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      await sut.addToCache('AString')
+      const pathToObjectDirectoryCache = await sut.getObjectCachePath('AString')
+      expect(pathToObjectDirectoryCache).not.undefined
+    })
+
+    it('should return undefined if object is not in cache', async () => {
+      const sut = new FsCache<string>({
+        addObjectToCacheDirectory: async (obj: string, dirPath: string) =>
+          Promise.resolve(),
+        objectToId: (obj: string) => obj,
+        rootCachePath: testRootCachePath,
+      })
+      const pathToObjectDirectoryCache = await sut.getObjectCachePath('AString')
+      expect(pathToObjectDirectoryCache).undefined
+    })
+  })
+})
