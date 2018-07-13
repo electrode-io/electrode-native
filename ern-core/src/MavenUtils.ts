16672c9b83cb689f59118a4d4dcd52ba72c94894
krunalsshah <krunal.shah.neu@gmail.com>
2018-07-13 01:49:06 -0700

    mavenConfig can take string and variable name in auth bean

--- ern-core/src/MavenUtils.ts
+++ ern-core/src/MavenUtils.ts
@@ -40,16 +40,28 @@ export default class MavenUtils {
       // Replace \ by \\ for Windows
       return `repository(url: "${mavenRepositoryUrl.replace(/\\/g, '\\\\')}")`
     } else if (repoType === 'http') {
-      mavenUser = mavenUser ? `'${mavenUser}'` : 'mavenUser'
-      mavenPassword = mavenPassword ? `'${mavenPassword}'` : 'mavenPassword'
+      // User can pass userName as "value" or variable [mavenUser]
+      const isMavenUserVar = mavenUser && mavenUser.lastIndexOf('[') === 0
+      // User can pass password as "value" or variable [mavenPassword]
+      const isMavenPwdVar =
+        mavenPassword && mavenPassword.lastIndexOf('[') === 0
       let authBlock = ''
-      if (mavenUser && mavenPassword) {
-        authBlock = `{ authentication(userName: ${mavenUser}, password: ${mavenPassword}) }`
-      } else if (mavenUser) {
-        authBlock = `{ authentication(userName: ${mavenUser}) }`
-      } else if (mavenPassword) {
-        authBlock = `{ authentication(password: ${mavenPassword}) }`
+      // Check if mavenUser or mavenPassword is to be appended as variable in the authentication bean
+      if (isMavenUserVar || isMavenPwdVar) {
+        authBlock = `{ authentication(userName: ${mavenUser!.slice(
+          1,
+          -1
+        )}, password: ${mavenPassword!.slice(1, -1)}) }`
+      } // Check if mavenUser or mavenPassword is to be appended as value in the authentication bean
+      else if (mavenUser || mavenPassword) {
+        authBlock = `{ authentication(userName: "${mavenUser}", password: "${mavenPassword}") }`
       }
+      // --config '{"mavenUser": "myUser","mavenPassword": "myPassword"}'
+      // Result : "repository(url: "http://domain.name:8081/repositories") { authentication(userName: "myUser", password: "myPassword") }”
+      // --config '{"mavenUser": "[myUserVar]","mavenPassword": "[myPasswordVar]”}'
+      // Result : "repository(url: "http://domain.name:8081/repositories") { authentication(userName: myUserVar, password: myPasswordVar) }”
+      // no config
+      // Result : "repository(url: "http://domain.name:8081/repositories")
       return `repository(url: "${mavenRepositoryUrl}") ${authBlock}`
     }
   }
