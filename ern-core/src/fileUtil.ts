f7926379e3e30a582aa010fd38e64c54e517a6ec
Benoit Lemaire <blemaire@walmartlabs.com>
2018-07-16 21:58:23 -0700

    Add isExecutable function to fileUtils

--- ern-core/src/fileUtil.ts
+++ ern-core/src/fileUtil.ts
@@ -89,3 +89,15 @@ export function writeJSONSync(filePath: string, obj: any): string {
 export function chmodr(fileMode: string, filePath: string) {
   shell.chmod('-R', fileMode, filePath)
 }
+
+/**
+ * Check if a given file is executable
+ * @param filePath Path to the file to check
+ */
+export async function isExecutable(filePath: string): Promise<boolean> {
+  return new Promise<boolean>(resolve => {
+    fs.access(filePath, fs.constants.X_OK, err => {
+      err ? resolve(false) : resolve(true)
+    })
+  })
+}
