cbbbf40e516faea586155ed1591d3230c8d7164f
krunalsshah <krunal.shah.neu@gmail.com>
2018-07-23 13:49:44 -0700

    add extension to the top level command

--- ern-local-cli/src/commands/binarystore.ts
+++ ern-local-cli/src/commands/binarystore.ts
@@ -4,7 +4,10 @@ export const command = 'binarystore'
 export const desc = 'Binary store access commands'
 export const builder = (argv: Argv) => {
   return argv
-    .commandDir('binarystore')
+    .commandDir('binarystore', {
+      extensions:
+        process.env.NODE_ENV === 'development' ? ['js', 'ts'] : ['js'],
+    })
     .demandCommand(1, 'binarystore needs a command')
     .strict()
 }
