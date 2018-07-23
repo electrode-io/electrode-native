b4c6840889c49a864fba1b8531e368346cd019e6
krunalsshah <krunal.shah.neu@gmail.com>
2018-07-23 16:06:51 -0700

    implement get config

--- ern-local-cli/src/commands/platform/config/get.ts
+++ ern-local-cli/src/commands/platform/config/get.ts
@@ -1,5 +1,6 @@
 import { Argv } from 'yargs'
 import utils from '../../../lib/utils'
+import { config as ernConfig, utils as coreUtils, log } from 'ern-core'
 
 export const command = 'get <key>'
 export const desc = 'Echo the config value to stdout'
@@ -8,6 +9,11 @@ export const builder = (argv: Argv) => {
   return argv.epilog(utils.epilog(exports))
 }
 
-export const handler = async () => {
-  return true
+export const handler = async ({ key }: { key: string }) => {
+  await utils.logErrorAndExitIfNotSatisfied({
+    isValidPlatformConfig: {
+      key,
+    },
+  })
+  log.info(`Configuration value for ${key} is ${ernConfig.getValue(key)}`)
 }
