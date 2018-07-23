f4a5dc0e0a78ea28368faff25bdf7d8a5e78befa
krunalsshah <krunal.shah.neu@gmail.com>
2018-07-23 13:49:44 -0700

    add extension to the top level command

--- ern-local-cli/src/commands/platform/plugins.ts
+++ ern-local-cli/src/commands/platform/plugins.ts
@@ -3,7 +3,12 @@ import { Argv } from 'yargs'
 export const command = 'plugins'
 export const desc = 'Plugins access commands'
 export const builder = (argv: Argv) => {
-  return argv.commandDir('plugins').demandCommand(1, 'plugins needs a command')
+  return argv
+    .commandDir('plugins', {
+      extensions:
+        process.env.NODE_ENV === 'development' ? ['js', 'ts'] : ['js'],
+    })
+    .demandCommand(1, 'plugins needs a command')
 }
 export const handler = (args: any) => {
   return
