8856092020c1b7770f6128c2d9994f6cde0b9d69
Benoit Lemaire <blemaire@walmartlabs.com>
2018-07-23 21:01:07 -0700

    v0.22.0

--- ern-local-cli/scripts/postinstall.js
+++ ern-local-cli/scripts/postinstall.js
@@ -16,7 +16,7 @@ const os = require('os')
 // ....
 // |_ .ernrc
 
-const PLATFORM_VERSION = '1000.0.0'
+const PLATFORM_VERSION = '0.22.0'
 // Path to ern platform root directory
 const ERN_PATH = process.env['ERN_HOME'] || path.join(os.homedir(), '.ern')
 // Path to ern global configuration file
@@ -24,23 +24,28 @@ const ERN_RC_GLOBAL_FILE_PATH = path.join(ERN_PATH, '.ernrc')
 // Path to ern platform manifest repo
 const ERN_MANIFEST_REPO_PATH = path.join(ERN_PATH, 'ern-master-manifest')
 // Remote git path to ern manifest repo
-const ERN_MANIFEST_DEFAULT_GIT_REPO = 'https://github.com/electrode-io/electrode-native-manifest.git'
+const ERN_MANIFEST_DEFAULT_GIT_REPO =
+  'https://github.com/electrode-io/electrode-native-manifest.git'
 
 if (!isGitInstalled()) {
-  throw new Error('git is not installed on this machine ! Please install git and retry.')
+  throw new Error(
+    'git is not installed on this machine ! Please install git and retry.'
+  )
 }
 
 // Clone ern manifest repository if not done already
 if (!fs.existsSync(ERN_MANIFEST_REPO_PATH)) {
   console.log('Cloning electrode-native master manifest')
-  execSync(`git clone ${ERN_MANIFEST_DEFAULT_GIT_REPO} ${ERN_MANIFEST_REPO_PATH}`)
+  execSync(
+    `git clone ${ERN_MANIFEST_DEFAULT_GIT_REPO} ${ERN_MANIFEST_REPO_PATH}`
+  )
 }
 
 // Generate initial ernrc file, if it doesnt already exists (i.e if this is not a first time platform install)
 if (!fs.existsSync(ERN_RC_GLOBAL_FILE_PATH)) {
   console.log(`=> Creating initial .ernrc configuration file`)
   const ernRc = {
-    platformVersion: PLATFORM_VERSION
+    platformVersion: PLATFORM_VERSION,
   }
   fs.writeFileSync(ERN_RC_GLOBAL_FILE_PATH, JSON.stringify(ernRc, null, 2))
 } else {
@@ -49,7 +54,7 @@ if (!fs.existsSync(ERN_RC_GLOBAL_FILE_PATH)) {
   // We should probably just patch the .ernrc file with any new configuration data introduced in this version
 }
 
-function isGitInstalled () {
+function isGitInstalled() {
   try {
     execSync('git --version')
     return true
