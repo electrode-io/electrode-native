b000f8fc2ac6f03abd13aeccaf870ee27340d314
Benoit Lemaire <blemaire@walmartlabs.com>
2018-07-24 20:24:07 -0700

    Remove collections dependency from ern-api-gen

--- ern-api-gen/src/auth/AuthParser.js
+++ ern-api-gen/src/auth/AuthParser.js
@@ -1,24 +1,30 @@
 import AuthorizationValue from '../models/auth/AuthorizationValue'
 import LoggerFactory from '../java/LoggerFactory'
-import {isNotEmpty} from '../java/StringUtils'
+import { isNotEmpty } from '../java/StringUtils'
 import StringBuilder from '../java/StringBuilder'
-import {URLDecoder, URLEncoder} from '../java/encoders'
+import { URLDecoder, URLEncoder } from '../java/encoders'
 
 export default class AuthParser {
-  static parse (urlEncodedAuthStr) {
+  static parse(urlEncodedAuthStr) {
     const auths = []
     if (isNotEmpty(urlEncodedAuthStr)) {
       for (const part of urlEncodedAuthStr.split(',')) {
         let kvPair = part.split(':', 2)
         if (kvPair.length === 2) {
-          auths.add(new AuthorizationValue(URLDecoder.decode(kvPair[0]), URLDecoder.decode(kvPair[1]), 'header'))
+          auths.push(
+            new AuthorizationValue(
+              URLDecoder.decode(kvPair[0]),
+              URLDecoder.decode(kvPair[1]),
+              'header'
+            )
+          )
         }
       }
     }
     return auths
   }
 
-  static reconstruct (authorizationValueList) {
+  static reconstruct(authorizationValueList) {
     if (authorizationValueList != null) {
       let b = new StringBuilder()
       for (const v of authorizationValueList) {
@@ -26,7 +32,9 @@ export default class AuthParser {
           if (b.toString().length > 0) {
             b.append(',')
           }
-          b.append(URLEncoder.encode(v.getKeyName(), 'UTF-8')).append(':').append(URLEncoder.encode(v.getValue(), 'UTF-8'))
+          b.append(URLEncoder.encode(v.getKeyName(), 'UTF-8'))
+            .append(':')
+            .append(URLEncoder.encode(v.getValue(), 'UTF-8'))
         } catch (e) {
           Log.trace(e)
         }
