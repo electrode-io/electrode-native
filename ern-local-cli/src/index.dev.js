e3cb221fb4a20a5ed2ef606f9989a76bd96b0faa
Benoit Lemaire <blemaire@walmartlabs.com>
2018-08-28 18:07:13 -0700

    :white_check_mark: Fix code coverage reporting

--- ern-local-cli/src/index.dev.js
+++ ern-local-cli/src/index.dev.js
@@ -3,31 +3,26 @@ const path = require('path')
 const Module = require('module')
 const oload = Module._load
 
-if (process.env.NODE_ENV === 'coverage-st') {
-  // tslint:disable-next-line:no-var-requires
-  require('../dist/index').default()
-} else {
-  process.env.NODE_ENV = 'development'
-  process.env.TS_NODE_PROJECT = path.resolve(
-    __dirname,
-    '..',
-    '..',
-    'tsconfig.json'
-  )
+process.env.NODE_ENV = 'development'
+process.env.TS_NODE_PROJECT = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'tsconfig.json'
+)
 
-  let tsNodeSourceMapSupportModule
-  Module._load = function(file, parent) {
-    if (file === 'source-map-support' && parent.id.includes('ts-node')) {
-      tsNodeSourceMapSupportModule = oload(file, parent)
-      return tsNodeSourceMapSupportModule
-    } else if (file === 'source-map-support') {
-      return tsNodeSourceMapSupportModule
-    } else {
-      return oload(file, parent)
-    }
+let tsNodeSourceMapSupportModule
+Module._load = function(file, parent) {
+  if (file === 'source-map-support' && parent.id.includes('ts-node')) {
+    tsNodeSourceMapSupportModule = oload(file, parent)
+    return tsNodeSourceMapSupportModule
+  } else if (file === 'source-map-support') {
+    return tsNodeSourceMapSupportModule
+  } else {
+    return oload(file, parent)
   }
-
-  require('tsconfig-paths/register')
-  require('ts-node').register({})
-  require('./index.ts').default()
 }
+
+require('tsconfig-paths/register')
+require('ts-node').register({})
+require('./index.ts').default()
