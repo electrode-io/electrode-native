32fe7573d37d2e2625f2764a1cb6bd08bc357513
Benoit Lemaire <blemaire@walmartlabs.com>
2018-08-29 21:05:21 -0700

    :hammer: Refactor publishers and transformers factory

--- ern-container-publisher/src/getPublisher.ts
+++ ern-container-publisher/src/getPublisher.ts
@@ -1,52 +1,16 @@
-import { PackagePath, Platform, shell, yarn, readPackageJson } from 'ern-core'
+import { ModuleFactory, Platform, PackagePath } from 'ern-core'
 import { ContainerPublisher } from './types'
-import fs from 'fs'
-import path from 'path'
 
 const ERN_PUBLISHER_PACKAGE_PREFIX = 'ern-container-publisher-'
-const REGISTRY_PATH_VERSION_RE = new RegExp(/^(.+)@(.+)$/)
+const ERN_PUBLISHER_CACHE_DIRECTORY = Platform.containerPublishersCacheDirectory
+
+const transformerFactory = new ModuleFactory<ContainerPublisher>(
+  ERN_PUBLISHER_PACKAGE_PREFIX,
+  ERN_PUBLISHER_CACHE_DIRECTORY
+)
 
 export default async function getPublisher(
   publisher: string
 ): Promise<ContainerPublisher> {
-  let pathToPublisherEntry
-  if (fs.existsSync(publisher)) {
-    const pathWithSrc = path.join(publisher, 'src')
-    pathToPublisherEntry = fs.existsSync(pathWithSrc) ? pathWithSrc : publisher
-  } else {
-    try {
-      shell.pushd(Platform.containerPublishersCacheDirectory)
-      if (
-        !publisher.startsWith('@') &&
-        !publisher.startsWith(ERN_PUBLISHER_PACKAGE_PREFIX)
-      ) {
-        publisher = `${ERN_PUBLISHER_PACKAGE_PREFIX}${publisher}`
-      }
-      const publisherPackagePath = PackagePath.fromString(publisher)
-      const packageJson = await readPackageJson(
-        Platform.containerPublishersCacheDirectory
-      )
-      if (
-        packageJson.dependencies &&
-        packageJson.dependencies[publisherPackagePath.basePath]
-      ) {
-        await yarn.upgrade(publisherPackagePath)
-      } else {
-        await yarn.add(publisherPackagePath)
-      }
-      const pkgName = REGISTRY_PATH_VERSION_RE.test(publisher)
-        ? REGISTRY_PATH_VERSION_RE.exec(publisher)![1]
-        : publisher
-      pathToPublisherEntry = path.join(
-        Platform.containerPublishersCacheDirectory,
-        'node_modules',
-        pkgName
-      )
-    } finally {
-      shell.popd()
-    }
-  }
-
-  const Publisher = require(pathToPublisherEntry).default
-  return new Publisher()
+  return transformerFactory.getModuleInstance(PackagePath.fromString(publisher))
 }
---
+++ ern-core/src/ModuleFactory.ts
@@ -0,0 +1,101 @@
+import fs from 'fs'
+import path from 'path'
+import shell from './shell'
+import { yarn } from './clients'
+import { PackagePath } from './PackagePath'
+import { readPackageJson } from './packageJsonFileUtils'
+
+export class ModuleFactory<T> {
+  public constructor(
+    readonly packagePrefix: string,
+    readonly packageCachePath: string
+  ) {}
+
+  public async getModuleInstance(p: PackagePath): Promise<T> {
+    if (!p.isFilePath && !p.isRegistryPath) {
+      throw new Error(`Not a supported package path : ${p.toString()}`)
+    }
+    const pathToModule = await this.getLocalPathToPackage(p)
+    return this.instantiateModule(pathToModule)
+  }
+
+  private instantiateModule(pathToModule: string): T {
+    const Module = require(pathToModule).default
+    return new Module()
+  }
+
+  private async getLocalPathToPackage(p: PackagePath): Promise<string> {
+    return p.isFilePath
+      ? this.getPathToLocalPackage(p)
+      : await this.getPathToRegistryPackage(p)
+  }
+
+  private getPathToLocalPackage(p: PackagePath): string {
+    const pathWithSrc = path.join(p.basePath, 'src')
+    return fs.existsSync(pathWithSrc) ? pathWithSrc : p.basePath
+  }
+
+  private async getPathToRegistryPackage(p: PackagePath): Promise<string> {
+    if (!this.doesPackageCacheExist()) {
+      await this.createPackageCache()
+    }
+
+    const modulePackagePath = this.processPackageRegistryPath(p)
+    await this.refreshCacheFor(modulePackagePath)
+
+    return path.join(
+      this.packageCachePath,
+      'node_modules',
+      modulePackagePath.basePath
+    )
+  }
+
+  private doesPackageCacheExist(): boolean {
+    return fs.existsSync(this.packageCachePath)
+  }
+
+  private async createPackageCache() {
+    shell.mkdir('-p', this.packageCachePath)
+    try {
+      shell.pushd(this.packageCachePath)
+      await yarn.init()
+    } finally {
+      shell.popd()
+    }
+  }
+
+  private async refreshCacheFor(p: PackagePath) {
+    const packageJson = await readPackageJson(this.packageCachePath)
+    return packageJson.dependencies && packageJson.dependencies[p.basePath]
+      ? this.upgradeCachedPackage(p)
+      : this.addPackageToCache(p)
+  }
+
+  private async addPackageToCache(p: PackagePath) {
+    shell.pushd(this.packageCachePath)
+    try {
+      await yarn.add(p)
+    } finally {
+      shell.popd()
+    }
+  }
+
+  private async upgradeCachedPackage(p: PackagePath) {
+    shell.pushd(this.packageCachePath)
+    try {
+      await yarn.upgrade(p)
+    } finally {
+      shell.popd()
+    }
+  }
+
+  private processPackageRegistryPath(p: PackagePath): PackagePath {
+    if (
+      !p.fullPath.startsWith('@') &&
+      !p.fullPath.startsWith(this.packagePrefix)
+    ) {
+      return PackagePath.fromString(`${this.packagePrefix}${p.fullPath}`)
+    }
+    return p
+  }
+}
