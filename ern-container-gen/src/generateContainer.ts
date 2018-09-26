aa1967d20f6199a3ab110d763ebeeb562e901674
Benoit Lemaire <blemaire@walmartlabs.com>
2018-09-26 11:40:29 -0700

    :bug: Fix unclosed tag error when using regen-container

--- ern-container-gen/src/generateContainer.ts
+++ ern-container-gen/src/generateContainer.ts
@@ -54,6 +54,8 @@ export async function generateContainer(
           generatedJsBundleOnly = true
         }
       }
+    } else {
+      shell.rm('-rf', path.join(config.outDir, '{.*,*}'))
     }
   }
 
