406733eafc51436073b649738a3349dc262c940e
Benoit Lemaire <blemaire@walmartlabs.com>
2018-10-15 12:05:49 -0700

    Move Cauldron logic from add nativeapp command to CauldronHelper class

--- ern-local-cli/src/commands/cauldron/add/nativeapp.ts
+++ ern-local-cli/src/commands/cauldron/add/nativeapp.ts
@@ -1,5 +1,5 @@
-import { PackagePath, NativeApplicationDescriptor, log, kax } from 'ern-core'
-import { CauldronHelper, getActiveCauldron } from 'ern-cauldron-api'
+import { getActiveCauldron } from 'ern-cauldron-api'
+import { NativeApplicationDescriptor, log, kax } from 'ern-core'
 import inquirer from 'inquirer'
 import _ from 'lodash'
 import {
@@ -49,95 +49,30 @@ export const commandHandler = async ({
   cauldron = await getActiveCauldron()
   await cauldron.beginTransaction()
 
-  const nativeApplicationDescriptor = new NativeApplicationDescriptor(
-    descriptor.name,
-    descriptor.platform
-  )
-
-  let previousApps
-  if (await cauldron.isDescriptorInCauldron(nativeApplicationDescriptor)) {
-    previousApps = await cauldron.getDescriptor(nativeApplicationDescriptor)
-  }
-
-  await kax.task(`Adding ${descriptor}`).run(cauldron.addDescriptor(descriptor))
-
-  if (previousApps && previousApps.versions.length > 0) {
-    const latestVersion: any = _.last(previousApps.versions)
-    const latestVersionName = latestVersion.name
-
-    if (copyFromVersion) {
-      if (copyFromVersion === 'latest') {
-        await kax
-          .task(`Copying data over from latest version ${latestVersionName}`)
-          .run(copyOverPreviousVersionData(descriptor, latestVersion, cauldron))
-      } else if (copyFromVersion === 'none') {
-        log.info(
-          `Skipping copy over from previous version as 'none' was specified`
-        )
-      } else {
-        const version = _.find(
-          previousApps.versions,
-          v => v.name === copyFromVersion
-        )
-        if (!version) {
-          throw new Error(
-            `Could not resolve native application version to copy Cauldron data from.\nExamine current value : ${copyFromVersion}`
-          )
-        }
-        await kax
-          .task(`Copying data over from version ${copyFromVersion}`)
-          .run(copyOverPreviousVersionData(descriptor, version, cauldron))
-      }
-    } else if (await askUserCopyPreviousVersionData(latestVersionName)) {
-      await kax
-        .task('Copying data over from previous version')
-        .run(copyOverPreviousVersionData(descriptor, latestVersion, cauldron))
+  if (copyFromVersion === 'none') {
+    copyFromVersion = undefined
+  } else if (
+    !copyFromVersion &&
+    (await cauldron.isDescriptorInCauldron(
+      new NativeApplicationDescriptor(descriptor.name, descriptor.platform)
+    ))
+  ) {
+    const mostRecentVersion = await cauldron.getMostRecentNativeApplicationVersion(
+      descriptor
+    )
+    if (await askUserCopyPreviousVersionData(mostRecentVersion.name)) {
+      copyFromVersion = mostRecentVersion.name
     }
   }
+
+  await kax
+    .task(`Adding ${descriptor}`)
+    .run(cauldron.addNativeApplicationVersion(descriptor, { copyFromVersion }))
 
   await kax
     .task('Updating Cauldron')
     .run(cauldron.commitTransaction(`Add ${descriptor} native application`))
   log.info(`${descriptor} successfully added to the the Cauldron`)
-}
-
-async function copyOverPreviousVersionData(
-  napDescriptor: NativeApplicationDescriptor,
-  nativeAppVersion: any,
-  cauldron: CauldronHelper
-) {
-  // Copy over previous native application version native dependencies
-  for (const nativeDep of nativeAppVersion.container.nativeDeps) {
-    await cauldron.addContainerNativeDependency(
-      napDescriptor,
-      PackagePath.fromString(nativeDep)
-    )
-  }
-  // Copy over previous native application version container MiniApps
-  for (const containerMiniApp of nativeAppVersion.container.miniApps) {
-    await cauldron.addContainerMiniApp(
-      napDescriptor,
-      PackagePath.fromString(containerMiniApp)
-    )
-  }
-  // Copy over previous yarn lock if any
-  if (nativeAppVersion.yarnLocks) {
-    await cauldron.setYarnLocks(napDescriptor, nativeAppVersion.yarnLocks)
-  }
-  // Copy over container version
-  if (nativeAppVersion.containerVersion) {
-    await cauldron.updateContainerVersion(
-      napDescriptor,
-      nativeAppVersion.containerVersion
-    )
-  }
-  // Copy over ern version
-  if (nativeAppVersion.container.ernVersion) {
-    await cauldron.updateContainerErnVersion(
-      napDescriptor,
-      nativeAppVersion.container.ernVersion
-    )
-  }
 }
 
 async function askUserCopyPreviousVersionData(
