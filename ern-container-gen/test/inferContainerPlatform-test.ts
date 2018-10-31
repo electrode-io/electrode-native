d4aabd8a3d5054848f9de6c379bc688c258358c1
Benoit Lemaire <lemaireb@gmail.com>
2018-10-31 12:06:37 -0700

    :white_check_mark: Add more unit tests to ern-container-gen

---
+++ ern-container-gen/test/inferContainerPlatform-test.ts
@@ -0,0 +1,24 @@
+import { doesThrow } from 'ern-util-dev'
+import { inferContainerPlatform } from '../src/inferContainerPlatform'
+import { assert, expect } from 'chai'
+import path from 'path'
+
+describe('inferContainerPlatform', () => {
+  it('should throw if the container path does not exist', () => {
+    expect(() => inferContainerPlatform('/does/not/exist')).to.throw()
+  })
+
+  it('should return android if the container is an android one', () => {
+    const result = inferContainerPlatform(
+      path.resolve('..', 'system-tests', 'fixtures', 'android-container')
+    )
+    expect(result).eql('android')
+  })
+
+  it('should return android if the container is an ios one', () => {
+    const result = inferContainerPlatform(
+      path.resolve('..', 'system-tests', 'fixtures', 'ios-container')
+    )
+    expect(result).eql('ios')
+  })
+})
