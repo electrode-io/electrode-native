9e628970b62a5bcf29cc6c22f1e922086dded7da
Benoit Lemaire <lemaireb@gmail.com>
2019-04-02 19:06:52 -0700

    :sparkles: Add new create-composite command

---
+++ ern-local-cli/src/commands/create-composite.ts
@@ -0,0 +1,139 @@
+import { generateMiniAppsComposite } from 'ern-container-gen'
+import {
+  PackagePath,
+  NativeApplicationDescriptor,
+  Platform,
+  log,
+  NativePlatform,
+} from 'ern-core'
+import { getActiveCauldron } from 'ern-cauldron-api'
+import {
+  epilog,
+  logErrorAndExitIfNotSatisfied,
+  tryCatchWrap,
+  askUserToChooseANapDescriptorFromCauldron,
+} from '../lib'
+import _ from 'lodash'
+import { Argv } from 'yargs'
+import fs from 'fs'
+import path from 'path'
+
+export const command = 'create-composite'
+export const desc = 'Create a JS composite project locally'
+
+export const builder = (argv: Argv) => {
+  return argv
+    .option('descriptor', {
+      alias: 'd',
+      describe: 'Full native application descriptor',
+      type: 'string',
+    })
+    .coerce('descriptor', d =>
+      NativeApplicationDescriptor.fromString(d, { throwIfNotComplete: true })
+    )
+    .option('extraJsDependencies', {
+      alias: 'e',
+      describe: 'Additional JS dependency(ies)',
+      type: 'array',
+    })
+    .coerce('extraJsDependencies', d => d.map(PackagePath.fromString))
+    .option('fromGitBranches', {
+      describe: 'Favor MiniApp(s) branches',
+      type: 'boolean',
+    })
+    .option('jsApiImpls', {
+      describe: 'One or more JS API implementation(s)',
+      type: 'array',
+    })
+    .coerce('jsApiImpls', d => d.map(PackagePath.fromString))
+    .option('miniapps', {
+      alias: 'm',
+      describe: 'One or more MiniApp(s)',
+      type: 'array',
+    })
+    .coerce('miniapps', d => d.map(PackagePath.fromString))
+    .option('outDir', {
+      alias: 'out',
+      describe: 'Output directory',
+      type: 'string',
+    })
+    .epilog(epilog(exports))
+}
+
+export const commandHandler = async ({
+  descriptor,
+  extraJsDependencies,
+  fromGitBranches,
+  jsApiImpls,
+  miniapps,
+  outDir,
+}: {
+  descriptor?: NativeApplicationDescriptor
+  extraJsDependencies?: PackagePath[]
+  fromGitBranches?: boolean
+  jsApiImpls?: PackagePath[]
+  miniapps?: PackagePath[]
+  outDir?: string
+  platform?: NativePlatform
+} = {}) => {
+  if (outDir && fs.existsSync(outDir)) {
+    if (fs.readdirSync(outDir).length > 0) {
+      throw new Error(
+        `${outDir} directory exists and is not empty.
+Output directory should either not exist (it will be created) or should be empty.`
+      )
+    }
+  }
+
+  const cauldron = await getActiveCauldron({ throwIfNoActiveCauldron: false })
+  if (!cauldron && !miniapps) {
+    throw new Error(
+      "A Cauldron must be active if you don't explicitly provide miniapps"
+    )
+  }
+
+  // Full native application selector was not provided.
+  // Ask the user to select a completeNapDescriptor from a list
+  // containing all the native applications versions in the cauldron
+  // Not needed if miniapps are directly provided
+  if (!descriptor && !miniapps) {
+    descriptor = await askUserToChooseANapDescriptorFromCauldron()
+  }
+
+  let pathToYarnLock
+  if (descriptor) {
+    await logErrorAndExitIfNotSatisfied({
+      napDescriptorExistInCauldron: {
+        descriptor,
+        extraErrorMessage:
+          'You cannot create a composite for a non-existing native application version.',
+      },
+    })
+    miniapps = await cauldron.getContainerMiniApps(descriptor, {
+      favorGitBranches: !!fromGitBranches,
+    })
+    jsApiImpls = await cauldron.getContainerJsApiImpls(descriptor)
+    const containerGenConfig = await cauldron.getContainerGeneratorConfig(
+      descriptor
+    )
+    if (!containerGenConfig || !containerGenConfig.bypassYarnLock) {
+      pathToYarnLock = await cauldron.getPathToYarnLock(descriptor, 'container')
+    } else {
+      log.debug(
+        'Bypassing yarn.lock usage as bypassYarnLock flag is set in config'
+      )
+    }
+  }
+
+  await generateMiniAppsComposite(
+    miniapps!,
+    outDir || path.join(Platform.rootDirectory, 'miniAppsComposite'),
+    {
+      extraJsDependencies,
+      pathToYarnLock,
+    },
+    jsApiImpls
+  )
+}
+
+export const handler = tryCatchWrap(commandHandler)
