3921833b003f29cccfa73cacf17b0915d1da7822
Benoit Lemaire <lemaireb@gmail.com>
2019-04-23 20:48:22 -0700

    Create temporary directory if it does not exist

--- ern-core/src/createTmpDir.ts
+++ ern-core/src/createTmpDir.ts
@@ -1,8 +1,13 @@
 import tmp from 'tmp'
 import config from './config'
+import shell from './shell'
+import fs from 'fs'
 
 export default function(): string {
   const tmpDir = config.getValue('tmp-dir')
+  if (!fs.existsSync(tmpDir)) {
+    shell.mkdir('-p', tmpDir)
+  }
   const retainTmpDir = config.getValue('retain-tmp-dir', false)
   return tmp.dirSync({
     dir: tmpDir,
