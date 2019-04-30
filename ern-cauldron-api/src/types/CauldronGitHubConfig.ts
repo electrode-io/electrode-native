bb73347d043f25ae144b23263857d3cf74334f02
Benoit Lemaire <lemaireb@gmail.com>
2019-04-30 12:20:39 -0700

    :sparkles: Add new github create-ref command

---
+++ ern-cauldron-api/src/types/CauldronGitHubConfig.ts
@@ -0,0 +1,7 @@
+export interface CauldronGitHubConfig {
+  /**
+   * Base custom GitHub Enterprise API url
+   * For ex : https://company-github-host/api/v3
+   */
+  baseUrl?: string
+}
---
+++ ern-local-cli/src/commands/github/create-ref.ts
@@ -0,0 +1,163 @@
+import { log, NativeApplicationDescriptor, PackagePath } from 'ern-core'
+import { getActiveCauldron } from 'ern-cauldron-api'
+import { createBranch, createTag } from 'ern-orchestrator'
+import {
+  askUserToChooseANapDescriptorFromCauldron,
+  epilog,
+  logErrorAndExitIfNotSatisfied,
+  tryCatchWrap,
+} from '../../lib'
+import _ from 'lodash'
+import { Argv } from 'yargs'
+import inquirer from 'inquirer'
+
+export const command = 'create-ref'
+export const desc = 'Creates a new branch/tag in multiple GitHub repositories'
+
+export const builder = (argv: Argv) => {
+  return argv
+    .option('--branch', {
+      describe: 'Name of the new branch to create',
+      type: 'string',
+    })
+    .option('--descriptor', {
+      describe:
+        'Native application version containing the packages to create a branch/tag for',
+      type: 'string',
+    })
+    .coerce('descriptor', d =>
+      NativeApplicationDescriptor.fromString(d, { throwIfNotComplete: true })
+    )
+    .option('--fromBranch', {
+      describe:
+        'Create the ref from the current tracked branches of the packages',
+      type: 'boolean',
+    })
+    .option('--fromTagOrSha', {
+      describe: 'Create the ref from the current tag/sha of the packages',
+      type: 'boolean',
+    })
+    .option('--jsApiImplsOnly', {
+      describe: 'Create the ref for JS API Implementations only',
+      type: 'boolean',
+    })
+    .option('--miniAppsOnly', {
+      describe: 'Create the ref for MiniApps only',
+      type: 'boolean',
+    })
+    .option('--tag', {
+      describe: 'Name of the new tag to create',
+      type: 'string',
+    })
+    .epilog(epilog(exports))
+}
+
+export const commandHandler = async ({
+  branch,
+  descriptor,
+  fromBranch,
+  fromTagOrSha,
+  jsApiImplsOnly,
+  miniAppsOnly,
+  tag,
+}: {
+  branch?: string
+  descriptor?: NativeApplicationDescriptor
+  fromBranch?: boolean
+  fromTagOrSha?: boolean
+  jsApiImplsOnly?: boolean
+  miniAppsOnly?: boolean
+  tag?: string
+} = {}) => {
+  if (!branch && !tag) {
+    const { branchOrTag } = await inquirer.prompt([
+      <inquirer.Question>{
+        choices: ['branch', 'tag'],
+        message: 'Which type of ref to create ?',
+        name: 'branchOrTag',
+        type: 'list',
+      },
+    ])
+    const { branchOrTagName } = await inquirer.prompt(<inquirer.Question>{
+      message: `Please input the new ${branchOrTag} name`,
+      name: 'branchOrTagName',
+      type: 'input',
+    })
+    if (branchOrTag === 'branch') {
+      branch = branchOrTagName
+    } else {
+      tag = branchOrTagName
+    }
+  }
+
+  if (!fromBranch && !fromTagOrSha) {
+    const { fromBranchOrFromTagSha } = await inquirer.prompt([
+      <inquirer.Question>{
+        choices: ['fromBranch', 'fromTagOrSha'],
+        message: 'From which source to create the ref ?',
+        name: 'fromBranchOrFromTagSha',
+        type: 'list',
+      },
+    ])
+    if (fromBranchOrFromTagSha === 'fromBranch') {
+      fromBranch = true
+    } else {
+      fromTagOrSha = true
+    }
+  }
+
+  descriptor =
+    descriptor ||
+    (await askUserToChooseANapDescriptorFromCauldron({
+      onlyNonReleasedVersions: true,
+    }))
+
+  await logErrorAndExitIfNotSatisfied({
+    isEnvVariableDefined: {
+      extraErrorMessage:
+        'ERN_GITHUB_TOKEN environment variable must be set, to use `ern github` commands',
+      name: 'ERN_GITHUB_TOKEN',
+    },
+    napDescriptorExistInCauldron: {
+      descriptor,
+      extraErrorMessage:
+        'This command cannot work on a non existing native application version',
+    },
+  })
+
+  const cauldron = await getActiveCauldron()
+
+  const packages: PackagePath[] = []
+  if (!jsApiImplsOnly) {
+    const miniAppsPackages = fromBranch
+      ? await cauldron.getContainerMiniAppsBranches(descriptor)
+      : await cauldron.getContainerMiniApps(descriptor)
+    if (miniAppsPackages.length === 0) {
+      log.info(
+        `No MiniApps ${
+          fromBranch ? 'branches' : 'tags/sha'
+        } found in Cauldron for ${descriptor}`
+      )
+    }
+    packages.push(...miniAppsPackages)
+  }
+  if (!miniAppsOnly) {
+    const jsApiImplsPackages = fromBranch
+      ? await cauldron.getContainerJsApiImplsBranches(descriptor)
+      : await cauldron.getContainerJsApiImpls(descriptor)
+    log.info(
+      `No JS API Implementations ${
+        fromBranch ? 'branches' : 'tags/sha'
+      } found in Cauldron for ${descriptor}`
+    )
+    packages.push(...jsApiImplsPackages)
+  }
+
+  if (branch) {
+    await createBranch({ name: branch, packages })
+  } else if (tag) {
+    await createTag({ name: tag, packages })
+  }
+}
+
+export const handler = tryCatchWrap(commandHandler)
