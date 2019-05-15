6c8910eee1cc51be48065fabd5b0468900e7c05e
Benoit Lemaire <lemaireb@gmail.com>
2019-05-15 10:46:05 -0700

    :bug: Fix CLI epilog documentation url

--- ern-local-cli/src/lib/epilog.ts
+++ ern-local-cli/src/lib/epilog.ts
@@ -7,10 +7,13 @@ export function epilog({ command }: { command: string }) {
   const version = `v${semver.major(Platform.currentVersion)}.${semver.minor(
     Platform.currentVersion
   )}`
-  const rootUrl = `https://native.electrode.io/v/${version}/cli-commands`
+  const rootUrl =
+    Platform.currentVersion === '1000.0.0'
+      ? `https://native.electrode.io/cli-commands`
+      : `https://native.electrode.io/v/${version}/cli-commands`
   const commandWithoutOptions = command.split(' ')[0]
   const idx = _.indexOf(process.argv, commandWithoutOptions)
-  let commandPath = _.slice(process.argv, 2, idx).join('/')
+  let commandPath = _.slice(process.argv, 2, idx).join('-')
   commandPath = commandPath ? `/${commandPath}` : ''
   return `More info about this command @ ${chalk.bold(
     `${rootUrl}${commandPath}/${commandWithoutOptions}.html`
