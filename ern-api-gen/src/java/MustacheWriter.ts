6d8f166aac5bba0d4bea1224d49874ef375a26f3
Benoit Lemaire <lemaireb@gmail.com>
2019-05-22 14:27:53 -0700

    :hammer: Convert leftover js file in ern-api-gen to ts

---
+++ ern-api-gen/src/java/MustacheWriter.ts
@@ -0,0 +1,70 @@
+import Mustache from 'mustache'
+import { isIterable } from './isIterable'
+
+export class MustacheWriter extends Mustache.Writer {
+  public renderSection(token, context, partials, originalTemplate) {
+    let buffer = ''
+    let value = context.lookup(token[1])
+
+    if (!value) {
+      return
+    }
+
+    if (isIterable(value)) {
+      const itr = value[Symbol.iterator]()
+      let first = itr.next()
+      let isFirst = true
+      while (!first.done) {
+        const next = itr.next()
+        if (first.value != null) {
+          const ctx = context.push(first.value, isFirst, next.done)
+          buffer += super.renderTokens(
+            token[4],
+            ctx,
+            partials,
+            originalTemplate
+          )
+        }
+        isFirst = false
+        first = next
+      }
+      return buffer
+    } else if (
+      typeof value === 'object' ||
+      typeof value === 'string' ||
+      typeof value === 'number'
+    ) {
+      buffer += super.renderTokens(
+        token[4],
+        context.push(value),
+        partials,
+        originalTemplate
+      )
+    } else if (typeof value === 'function') {
+      if (typeof originalTemplate !== 'string') {
+        throw new Error(
+          'Cannot use higher-order sections without the original template'
+        )
+      }
+
+      // Extract the portion of the original template that the section contains.
+      value = value.call(
+        context.view,
+        originalTemplate.slice(token[3], token[5]),
+        template => super.render(template, context, partials)
+      )
+
+      if (value != null) {
+        buffer += value
+      }
+    } else {
+      buffer += super.renderTokens(
+        token[4],
+        context,
+        partials,
+        originalTemplate
+      )
+    }
+    return buffer
+  }
+}
