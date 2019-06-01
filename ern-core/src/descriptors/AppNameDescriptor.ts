62224c580e11651f80d9c6b7cefbd47b00756cb4
Benoit Lemaire <lemaireb@gmail.com>
2019-06-01 10:52:08 -0700

    Replace NativeApplicationDescriptor with narrowed types

---
+++ ern-core/src/descriptors/AppNameDescriptor.ts
@@ -0,0 +1,20 @@
+export class AppNameDescriptor {
+  public static fromString(s: string): AppNameDescriptor {
+    if (s.includes(':')) {
+      throw new Error(
+        `An AppNameDescriptor literal cannot contain the ':' reserved character.`
+      )
+    }
+    return new AppNameDescriptor(s)
+  }
+
+  constructor(public readonly name: string) {}
+
+  public toAppNameDescriptor(): AppNameDescriptor {
+    return this
+  }
+
+  public toString() {
+    return this.name
+  }
+}
