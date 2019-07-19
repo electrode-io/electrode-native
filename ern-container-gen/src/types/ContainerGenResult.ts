9c75397bb301131388f82c6cb73d2a0e2d9f62c4
Benoit Lemaire <lemaireb@gmail.com>
2019-07-18 17:55:17 -0700

    :hammer: Return container config in ContainerGenResult

--- ern-container-gen/src/types/ContainerGenResult.ts
+++ ern-container-gen/src/types/ContainerGenResult.ts
@@ -1,8 +1,13 @@
 import { BundlingResult } from 'ern-core'
+import { ContainerGeneratorConfig } from './ContainerGeneratorConfig'
 
 export interface ContainerGenResult {
   /**
    * Metadata resulting from the bundling
    */
   bundlingResult: BundlingResult
+  /**
+   * Configuration used to generate the Container
+   */
+  config: ContainerGeneratorConfig
 }
