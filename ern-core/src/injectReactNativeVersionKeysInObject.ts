6766f7105a9fc7cfb433d41fbd9c571b9305eac9
Benoit Lemaire <lemaireb@gmail.com>
2019-07-23 14:09:54 -0700

    :bug: Fix Android runner gen for pre 64bits

--- ern-core/src/injectReactNativeVersionKeysInObject.ts
+++ ern-core/src/injectReactNativeVersionKeysInObject.ts
@@ -7,8 +7,10 @@ export function injectReactNativeVersionKeysInObject(
   return Object.assign(object, {
     RN_VERSION_GTE_45: semver.gte(reactNativeVersion, '0.45.0'),
     RN_VERSION_GTE_54: semver.gte(reactNativeVersion, '0.54.0'),
+    RN_VERSION_GTE_59: semver.gte(reactNativeVersion, '0.59.0'),
     RN_VERSION_LT_54: semver.lt(reactNativeVersion, '0.54.0'),
     RN_VERSION_LT_58: semver.lt(reactNativeVersion, '0.58.0-rc.2'),
+    RN_VERSION_LT_59: semver.lt(reactNativeVersion, '0.59.0'),
     reactNativeVersion,
   })
 }
