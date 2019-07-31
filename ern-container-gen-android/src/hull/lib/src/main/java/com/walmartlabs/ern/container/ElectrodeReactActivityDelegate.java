24789464e117cc6c4366977f8ecbf4fbdbf16dac
Deepu Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2019-07-31 11:58:20 -0700

    Ensure the rootViewHolder is removed from the datastore when all the react views are unmounted for a component.

--- ern-container-gen-android/src/hull/lib/src/main/java/com/walmartlabs/ern/container/ElectrodeReactActivityDelegate.java
+++ ern-container-gen-android/src/hull/lib/src/main/java/com/walmartlabs/ern/container/ElectrodeReactActivityDelegate.java
@@ -164,11 +164,14 @@ public class ElectrodeReactActivityDelegate extends ReactActivityDelegate {
      * @param componentName {@link String} React native app view component name
      * @deprecated use {@link #removeMiniAppView(String, ReactRootView)}.
      */
-    @Deprecated
+     @Deprecated
     public void removeMiniAppView(@NonNull String componentName) {
         ReactRootViewHolder reactRootViewHolder = mReactRootViews.get(componentName);
         if (reactRootViewHolder != null) {
             reactRootViewHolder.remove(componentName);
+            if(reactRootViewHolder.size() == 0) {
+                mReactRootViews.remove(componentName);
+            }
         }
     }
 
@@ -176,6 +179,9 @@ public class ElectrodeReactActivityDelegate extends ReactActivityDelegate {
         ReactRootViewHolder reactRootViewHolder = mReactRootViews.get(componentName);
         if (reactRootViewHolder != null) {
             reactRootViewHolder.remove(rootView);
+            if(reactRootViewHolder.size() == 0) {
+                mReactRootViews.remove(componentName);
+            }
         }
     }
 
