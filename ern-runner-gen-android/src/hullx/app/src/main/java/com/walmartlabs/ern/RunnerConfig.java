8fa0e87db4c3c85b5c5d4a9bea52f0dc8c2b476e
Deepu Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2019-08-05 16:49:55 -0700

    add a new android runner project for MiniApps that uses ern-navigation framework.

---
+++ ern-runner-gen-android/src/hullx/app/src/main/java/com/walmartlabs/ern/RunnerConfig.java
@@ -0,0 +1,16 @@
+package com.walmartlabs.ern;
+
+//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// GENERATED RUNNER CONFIGURATION
+// DO NOT EDIT MANUALLY
+// CHANGES TO THIS FILE WILL BE OVERWRITTEN BY NEXT RUN OF ern run-android COMMAND
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+import com.walmartlabs.ern.container.miniapps.{{{pascalCaseMiniAppName}}}Activity;
+final class RunnerConfig {
+  static final Class MAIN_MINIAPP_ACTIVITY_CLASS =  {{{pascalCaseMiniAppName}}}Activity.class;
+  static final boolean RN_DEV_SUPPORT_ENABLED = {{{isReactNativeDevSupportEnabled}}};
+  static final String PACKAGER_HOST = "{{{packagerHost}}}";
+  static final String PACKAGER_PORT = "{{{packagerPort}}}";
+}
