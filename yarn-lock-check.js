e9510dea9ad2e3ddb26f8020679a6a5d7abdff7d
Frieder Bluemle <frieder.bluemle@walmartlabs.com>
2019-09-10 17:33:47 -0700

    Remove lock files from packages

--- yarn-lock-check.js
+++ yarn-lock-check.js
@@ -1,38 +1,24 @@
+const chalk = require('chalk')
 const fs = require('fs')
 const lockfile = require('@yarnpkg/lockfile')
-const chalk = require('chalk')
 const path = require('path')
 
-const yarnLock = 'yarn.lock'
-const yarnRegistryRe = /https:\/\/registry.yarnpkg.com|http:\/\/registry.npmjs.org/
-
-const yarnLockContent = fs.readFileSync(yarnLock, 'utf8')
+const yarnLockContent = fs.readFileSync('yarn.lock', 'utf8')
 const yarnLockParsed = lockfile.parse(yarnLockContent)
 
-/*
-  {
-  type: 'success',
-  object: {
-  'JSONSelect@0.4.0': {
-  version: '0.4.0',
-  resolved: 'https://registry.yarnpkg.com/JSONSelect/-/JSONSelect-0.4.0.tgz#a08edcc67eb3fcbe99ed630855344a0cf282bb8d'
-  }
-  }
-  */
+const allowedRegistriesRe = /https:\/\/registry.yarnpkg.com|http:\/\/registry.npmjs.org/
+
 if (yarnLockParsed && yarnLockParsed.object) {
   let incorrectRegistries = []
   for (let key of Object.keys(yarnLockParsed.object)) {
     if (yarnLockParsed.object[key]) {
       let resolved = yarnLockParsed.object[key].resolved
-      if (resolved && !yarnRegistryRe.exec(resolved)) {
+      if (resolved && !allowedRegistriesRe.exec(resolved)) {
         incorrectRegistries.push(resolved)
       }
     }
   }
 
-  /*
-    yarn.lock contains registry other than https://registry.yarnpkg.com
-    */
   if (incorrectRegistries.length > 0) {
     console.log(
       chalk.bold.red(
@@ -43,5 +29,9 @@ if (yarnLockParsed && yarnLockParsed.object) {
       console.log(chalk.bold.red(item))
     })
     process.exit(1)
+  } else {
+    console.log(
+      chalk.bold.green('✅ success')
+    )
   }
 }
