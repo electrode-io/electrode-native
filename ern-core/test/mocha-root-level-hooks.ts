e158aa94b515543745c3ccf690f340ac8a004280
Benoit Lemaire <lemaireb@gmail.com>
2019-11-11 16:30:12 -0800

    :wrench: Disable logging during unit tests run

---
+++ ern-core/test/mocha-root-level-hooks.ts
@@ -0,0 +1,40 @@
+//
+// This file contains any Mocha root level hooks
+// which should be run for any test cases regardless
+// of the file they live in.
+
+import shell from '../src/shell'
+import log from '../src/log'
+import { LogLevel } from '../src/coloredLog'
+import kax from '../src/kax'
+import { KaxRenderer, KaxTask } from 'kax'
+
+class KaxNullRenderer implements KaxRenderer {
+  public renderWarning(msg: string) {
+    // noop
+  }
+  public renderInfo(msg: string) {
+    // noop
+  }
+  public renderError(msg: string) {
+    // noop
+  }
+  public renderRaw(msg: string) {
+    // noop
+  }
+  public renderTask<T>(msg: string, task: KaxTask<T>) {
+    // noop
+  }
+}
+
+//
+// Before any test suite is run
+before(() => {
+  // Disable shell commands logging
+  shell.config.verbose = false
+  shell.config.silent = true
+  // Disable base logging (log.)
+  log.setLogLevel(LogLevel.Off)
+  // Disable kax logging (kax.)
+  kax.renderer = new KaxNullRenderer()
+})
