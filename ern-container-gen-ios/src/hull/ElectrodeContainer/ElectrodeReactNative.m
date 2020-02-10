fa70289c2098c3794be550397edaa824117e5646
Yihan Xing <yihan.xing@walmartlabs.com>
2020-02-10 13:53:02 -0800

    add rctModuleDidInitialize delegate function

--- ern-container-gen-ios/src/hull/ElectrodeContainer/ElectrodeReactNative.m
+++ ern-container-gen-ios/src/hull/ElectrodeContainer/ElectrodeReactNative.m
@@ -311,6 +311,7 @@ static NSString *enableBundleStore = @"enableBundleStore";
             if ([localModuleInstance  respondsToSelector:selector]) {
                 NSLog(@"RCTDidInitializeModuleNotification received");
                 ((void (*)(id, SEL))[localModuleInstance methodForSelector:selector])(localModuleInstance, selector);
+                [self.ernDelegate rctModuleDidInitialize];
             }
             if ([localModuleInstance  respondsToSelector:transceiverReadySelector]) {
                 ((void (*)(id, SEL))[localModuleInstance methodForSelector:transceiverReadySelector])(localModuleInstance, transceiverReadySelector);
