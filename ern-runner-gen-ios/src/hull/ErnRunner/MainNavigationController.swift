5b8a90bb2e37538ee0fab74c5b8dce90c083681f
Jeffrey Wang <jw2175@gmail.com>
2021-04-20 17:05:07 -0700

    Update ios runner to use Swift 5

--- ern-runner-gen-ios/src/hull/ErnRunner/MainNavigationController.swift
+++ ern-runner-gen-ios/src/hull/ErnRunner/MainNavigationController.swift
@@ -1,12 +1,9 @@
 /*
- * Copyright 2017 WalmartLabs
-
+ * Copyright 2021 WalmartLabs
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-
  * http://www.apache.org/licenses/LICENSE-2.0
-
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +11,17 @@
  * limitations under the License.
  */
 
-extern NSString *const MainMiniAppName;
-extern BOOL const RnDevSupportEnabled;
-extern NSString *const PackagerHost;
-extern NSString *const PackagerPort;
+import Foundation
+import ElectrodeContainer
+
+class MainNavigationController: ENBaseNavigationController {
+
+    override func getRootComponentName() -> String {
+        return RunnerConfig.mainMiniAppName
+    }
+
+    override func showCloseBarButton() -> Bool {
+        return false
+    }
+
+}
--- ern-runner-gen-ios/src/hull/ErnRunner/RunnerConfig.swift
+++ ern-runner-gen-ios/src/hull/ErnRunner/RunnerConfig.swift
@@ -1,12 +1,9 @@
 /*
- * Copyright 2017 WalmartLabs
-
+ * Copyright 2021 WalmartLabs
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-
  * http://www.apache.org/licenses/LICENSE-2.0
-
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,9 +17,11 @@
 // CHANGES TO THIS FILE WILL BE OVERWRITTEN BY NEXT RUN OF ern run-ios COMMAND
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-#import <UIKit/UIKit.h>
+import Foundation
 
-NSString *const MainMiniAppName = @"{{{miniAppName}}}";
-BOOL const RnDevSupportEnabled = {{{isReactNativeDevSupportEnabled}}};
-NSString *const PackagerHost = @"{{{packagerHost}}}";
-NSString *const PackagerPort = @"{{{packagerPort}}}";
+struct RunnerConfig {
+    static let mainMiniAppName = "{{{miniAppName}}}"
+    static let rnDevSupportEnabled = {{{isReactNativeDevSupportEnabled}}}
+    static let packagerHost = "{{{packagerHost}}}"
+    static let packagerPort = "{{{packagerPort}}}"
+}
